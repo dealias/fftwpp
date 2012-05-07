dccd9a5125cf129b10102151f13f32934b30ee1f
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-05-07 21:04:18 +0000

    added ratio.asy

--- trunk/fftw++/fftw++.cc
+++ trunk/fftw++/fftw++.cc
@@ -8,6 +8,7 @@ bool fftw::Wise=false;
 const double fftw::twopi=2.0*acos(-1.0);
 
 // User settings:
-unsigned int fftw::effort=FFTW_MEASURE;
+  //unsigned int fftw::effort=FFTW_MEASURE;
+unsigned int fftw::effort=FFTW_PATIENT;
 const char *fftw::WisdomName="wisdom3.txt";
 }
