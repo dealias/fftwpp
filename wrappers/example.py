2ab50f9c6fae54bca6da238d9da14ad78d103c3c
miroberts <malcolm.i.w.roberts@gmail.com>
2012-09-13 13:55:30 +0000

    Progress on python wrapper and examples.

--- wrappers/example.py
+++ wrappers/example.py
@@ -3,70 +3,77 @@ import fftwpp
 
 N = 8
 
-f = np.zeros(N)
-g = np.zeros(N)
+def init(f,g):
+    k=0
+    while k < len(f) :
+        f[k]=np.complex(k,k+1)
+        g[k]=np.complex(k,2*k+1)
+        k += 1
+    return;
 
+f = np.empty(N,'complex')
+g = np.empty(N,'complex')
 
-i=0
-while i < len(f) :
-    print i
-    f[i]=i
-    g[i]=i*i
-    i += 1
-    
+init(f,g)
 
+print "input:"
+print
+print f
+print
+print g
+
+print
+print "non-centered convolution:"
 conv = fftwpp.Convolution(f.shape)
-conv.convolve(f, g)
+conv.convolve(f,g)
+print f
+
+init(f,g)
+
+print
+print "centered convolution:"
+hconv = fftwpp.HConvolution(f.shape)
+hconv.convolve(f,g)
 print f
 
 
-L = 2*np.pi
 
-x, y = np.mgrid[0.0:L:L/N, 0.0:L:L/N]
+print
+print "2d convolutions:"
 
+def init2(f,g):
+    a=0
+    while a < f.shape[0] :
+        b=0
+        while b < f[0].shape[0] :
+            f[a][b]=np.complex(a,b)
+            g[a][b]=np.complex(2*a,b+1)
+            b += 1
+        a += 1
+    return;
+
+mx=4
+my=4
+x = np.ndarray(shape=(mx,my), dtype=complex)
+y = np.ndarray(shape=(mx,my), dtype=complex)
+
+init2(x,y)
+
+print
+print "non-centered convolution:"
+conv = fftwpp.Convolution(x.shape)
+conv.convolve(x,y)
+print x
+
+
+# FIXME: 2D Hermitian convolution wrapper is broken
+
+#init2(x,y)
+
+#print
+#print "centered convolution:"
+#conv = fftwpp.HConvolution(x.shape)
+#conv.convolve(x,y)
 #print x
 
-z1 = np.fft.fftn(np.sin(x))
-z2 = np.fft.fftn(np.sin(5*y))
 
-print z1.shape
-
-conv = fftwpp.Convolution(z1.shape)
-conv.convolve(z1, z2)
-z1 = z1 / N**2
-
-z3 = np.fft.fftn(np.sin(x)*np.sin(5*y))
-
-# check if the arrays are close
-#print np.allclose(z3, z1)
-
-N = 16
-L = 2*np.pi
-x, y, z = np.mgrid[0.0:L:L/N, 0.0:L:L/N, 0.0:L:L/N]
-f = np.fft.fftn(np.sin(x)*np.sin(z))
-g = np.fft.fftn(np.sin(5*y))
-
-assert f.shape == (N, N, N)
-assert g.shape == (N, N, N)
-
-c = fftwpp.Convolution(f.shape)
-c.convolve(f, g)
-
-
-# check if the arrays are close
-#print np.allclose(f/N**3, np.fft.fftn(np.sin(x)*np.sin(5*y)*np.sin(z)))
-
-
-# x = np.arange(0.0, L, L/N)
-# f = np.fft.fft(np.sin(x))
-# g = np.fft.fft(np.sin(5*x))
-
-# assert f.shape == (N,)
-
-# c = fftwpp.Convolution(f.shape)
-# c.convolve(f, g)
-# f = f / N
-
-# print np.allclose(f, np.fft.fftn(np.sin(x)*np.sin(5*x)))
-# print f
-# print np.fft.fft(np.sin(x)*np.sin(5*x))
--- wrappers/fftwpp.py
+++ wrappers/fftwpp.py
@@ -8,7 +8,7 @@ import numpy as np
 from numpy.ctypeslib import ndpointer
 from ctypes import *
 
-__all__ = [ 'Convolution' ] #, 'HConvolution' ]
+__all__ = [ 'Convolution' , 'HConvolution' ]
 
 # load fftwpp shared library
 clib = CDLL('./_fftwpp.so')
@@ -57,7 +57,8 @@ clib.fftwpp_conv3d_convolve.argtypes = [ c_void_p,
 # class HConvolution(object):
 #     """Implicitly zero-padded Hermitian convolution class.
 
-#     :param shape: shape/number of elements in the input arrays (int, tuple or list)
+#     :param shape: shape/number of elements in the input arrays (int,
+#     tuple or list)
 
 #     The length of *shape* determines the dimension of the convolution.
 
@@ -303,6 +304,144 @@ class Convolution(object):
             self.cptr = clib.fftwpp_create_conv3d(*shape)
             self._convolve = clib.fftwpp_conv3d_convolve
             self._delete = clib.fftwpp_conv3d_delete
+        else:
+            raise ValueError("invalid shape (length/dimension should be 1, 2, or 3)")
+
+
+    def __del__(self):
+        self._delete(self.cptr)
+
+
+    def convolve(self, f, g):
+        """Compute the convolution of *f* and *g*.
+
+        The convolution is performed in-place (*f* is over-written).
+        """
+
+        assert f.shape == self.shape
+        assert g.shape == self.shape
+
+        self._convolve(self.cptr, f, g)
+
+class HConvolution(object):
+    """Implicitly zero-padded complex convolution class. FIXME
+
+    :param shape: shape/number of elements in the input arrays (int, tuple or list)
+
+    The length of *shape* determines the dimension of the convolution.
+
+
+    One dimensional convolutions
+    ----------------------------
+
+    To perform one dimensional convolutions::
+
+    >>> import numpy as np
+    >>> import fftwpp
+    >>> N = 32
+    >>> L = 2*np.pi
+    >>> x = np.arange(0.0, L, L/N)
+    >>> f = np.fft.fftn(np.sin(x))
+    >>> g = np.fft.fftn(np.sin(5*x))
+
+    At this point, both ``f`` and ``g`` have shape ``(N,)``::
+
+    >>> assert f.shape == (N,)
+
+    Now, construct the convolution object and convolve::
+
+    >>> c = fftwpp.Convolution(f.shape)
+    >>> c.convolve(f, g)
+
+    The convolution is now in ``f``::
+
+    # >>> # np.allclose(np.fft.ifft(f)/32, np.sin(x)*np.sin(5*x))
+    # # True
+
+
+    Two dimensional convolutions
+    ----------------------------
+
+    To perform two dimensional convolutions::
+
+    >>> import numpy as np
+    >>> import fftwpp
+    >>> N = 32
+    >>> L = 2*np.pi
+    >>> x, y = np.mgrid[0.0:L:L/N, 0.0:L:L/N]
+    >>> f = np.fft.fftn(np.sin(x))
+    >>> g = np.fft.fftn(np.sin(5*y))
+
+    At this point, both ``f`` and ``g`` have shape ``(N, N)``::
+
+    >>> assert f.shape == (N, N)
+    >>> assert g.shape == (N, N)
+
+    Now, construct the convolution object and convolve::
+
+    >>> c = fftwpp.Convolution(f.shape)
+    >>> c.convolve(f, g)
+
+    Again, the convolution is now in ``f``::
+
+    >>> np.allclose(f/N**2, np.fft.fftn(np.sin(x)*np.sin(5*y)))
+    True
+
+
+    Three dimensional convolutions
+    ------------------------------
+
+    To perform three dimensional convolutions::
+
+    >>> import numpy as np
+    >>> import fftwpp
+    >>> N = 32
+    >>> L = 2*np.pi
+    >>> x, y, z = np.mgrid[0.0:L:L/N, 0.0:L:L/N, 0.0:L:L/N]
+    >>> f = np.fft.fftn(np.sin(x)*np.sin(z))
+    >>> g = np.fft.fftn(np.sin(5*y))
+
+    At this point, both ``f`` and ``g`` have shape ``(N, N, N)``::
+
+    >>> assert f.shape == (N, N, N)
+    >>> assert g.shape == (N, N, N)
+
+    Now, construct the convolution object and convolve::
+
+    >>> c = fftwpp.Convolution(f.shape)
+    >>> c.convolve(f, g)
+
+    Again, the convolution is now in ``f``::
+
+    >>> np.allclose(f/N**3, np.fft.fftn(np.sin(x)*np.sin(5*y)*np.sin(z)))
+    True
+
+
+    """
+
+    def __init__(self, shape):
+
+        if isinstance(shape, int):
+            shape = (shape,)
+
+        self.dim   = len(shape)
+        self.shape = tuple(shape)
+
+        if self.dim == 1:
+#             self.cptr = clib.fftwpp_create_hconv1d(c_int(shape[0]))
+            self.cptr = clib.fftwpp_create_hconv1d(*shape)
+            self._convolve = clib.fftwpp_hconv1d_convolve
+            self._delete = clib.fftwpp_hconv1d_delete
+        elif self.dim == 2:
+#             self.cptr = clib.fftwpp_create_hconv2d(c_int(ny), c_int(ny))
+            self.cptr = clib.fftwpp_create_hconv2d(*shape)
+            self._convolve = clib.fftwpp_hconv2d_convolve
+            self._delete = clib.fftwpp_hconv2d_delete
+        elif self.dim == 3:
+#             self.cptr = clib.fftwpp_create_hconv3d(c_int(shape[0]/2), c_int(shape[1]/2), c_int(shape[2]))
+            self.cptr = clib.fftwpp_create_hconv3d(*shape)
+            self._convolve = clib.fftwpp_hconv3d_convolve
+            self._delete = clib.fftwpp_hconv3d_delete
         else:
             raise ValueError("invalid shape (length/dimension should be 1, 2, or 3)")
 
