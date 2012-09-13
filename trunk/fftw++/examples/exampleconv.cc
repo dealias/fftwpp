3ddf6bb8019b7a8ac58406985ce3adb59b91a046
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-09-13 13:55:30 +0000

    Progress on python wrapper and examples.

--- trunk/fftw++/examples/exampleconv.cc
+++ trunk/fftw++/examples/exampleconv.cc
@@ -1,30 +1,41 @@
 #include "Complex.h"
 #include "convolution.h"
+#include "Array.h"
 
 // Compile with:
 // g++ -fopenmp exampleconv.cc convolution.cc fftw++.cc -lfftw3 -lfftw3_omp
 
 using namespace std;
+using namespace Array;
 using namespace fftwpp;
 
 // size of problem
 unsigned int m=8;
 
+unsigned int mx=4;
+unsigned int my=4;
+
 inline void init(Complex *f, Complex *g) 
 {
   unsigned int m1=m+1;
   for(unsigned int i=0; i < m1; i += m1) {
-    double s=sqrt(1.0+i);
-    double ffactor=(1.0+i)*s;
-    double gfactor=1.0/(1.0+i);
     Complex *fi=f+i;
     Complex *gi=g+i;
-    fi[0]=1.0*ffactor;
-    for(unsigned int k=1; k < m; k++) fi[k]=ffactor*Complex(k,k+1);
-    gi[0]=2.0*gfactor;
-    for(unsigned int k=1; k < m; k++) gi[k]=gfactor*Complex(k,2*k+1);
+    for(unsigned int k=0; k < m; k++) fi[k]=Complex(k,k+1);
+    for(unsigned int k=0; k < m; k++) gi[k]=Complex(k,2*k+1);
   }
 }
+
+inline void init(array2<Complex>& f, array2<Complex>& g) 
+{
+    for(unsigned int i=0; i < mx; ++i) {
+      for(unsigned int j=0; j < my; j++) {
+        f[i][j]=Complex(i,j);
+        g[i][j]=Complex(2*i,j+1);
+      }
+    }
+}
+  
 
 int main(int argc, char* argv[])
 {
@@ -34,35 +45,50 @@ int main(int argc, char* argv[])
   fftw::effort |= FFTW_NO_SIMD;
 #endif  
   
-  cout << "size m=" << m << endl;
-  
-  // allocate arrays:
-  Complex *f=ComplexAlign(m);
-  Complex *g=ComplexAlign(m);
-
-  // initialize arrays:
-  init(f,g);
-  cout << endl << "input:" << endl;
-  for(unsigned int i=0; i < m; i++) 
-    cout << f[i] << "\t" << g[i] << endl;
-  
-  {
-    cout << endl << "non-centered complex convolution:" << endl;
-    ImplicitConvolution C(m);
-    C.convolve(f,g);
-    for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
+  { // 1D convolutions:
+    cout << "size m=" << m << endl;
+    
+    // allocate arrays:
+    Complex *f=ComplexAlign(m);
+    Complex *g=ComplexAlign(m);
+    
+    // initialize arrays:
+    init(f,g);
+    cout << endl << "input:" << endl;
+    cout << "f\tg" << endl;
+    for(unsigned int i=0; i < m; i++) 
+      cout << f[i] << "\t" << g[i] << endl;
+    
+    {
+      cout << endl << "non-centered complex convolution:" << endl;
+      ImplicitConvolution C(m);
+      C.convolve(f,g);
+      for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
+    }
+    
+    init(f,g);
+    {
+      cout << endl << "centered Hermitian-symmetric convolution:" << endl;
+      ImplicitHConvolution C(m);
+      C.convolve(f,g);
+      for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
+    }
+    
+    deleteAlign(g);
+    deleteAlign(f);
   }
 
-  init(f,g);
-  {
-    cout << endl << "centered Hermitian-symmetric convolution:" << endl;
-    ImplicitHConvolution C(m);
+  { // 2D convolutions:
+    cout << endl << "2D non-centered complex convolution:" << endl;
+    size_t align=sizeof(Complex);
+    array2<Complex> f(mx,my,align);
+    array2<Complex> g(mx,my,align);
+    init(f,g);
+    cout << f << endl;
+    ImplicitConvolution2 C(mx,my);
     C.convolve(f,g);
-    for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
+    cout << f << endl;
   }
-
-  deleteAlign(g);
-  deleteAlign(f);
 
   return 0;
 }
