1ec1be141211899b064f9e207aefe011605d74b2
miroberts <malcolm.i.w.roberts@gmail.com>
2012-09-25 14:26:39 +0000

    work on c wrapper

--- wrappers/cfftw++.h
+++ wrappers/cfftw++.h
@@ -10,28 +10,25 @@
 #ifndef CFFTWPP_H
 #define CFFTWPP_H
 
+/* #include<complex.h> */
+
 typedef struct Complex Complex;
 
 typedef struct ImplicitConvolution ImplicitConvolution;
 typedef struct ImplicitHConvolution ImplicitHConvolution;
 
-#ifdef  __cplusplus
+/* FIXME: */
+/*  expected ‘double *’ but argument is of type ‘struct Complex *’ */
+/* Complex *create_ComplexAlign(unsigned int n); */
 
-#include "fftw++.h"
-
-Complex *create_ComplexAlign(unsigned int n);
-void init(Complex *f, unsigned int n); // temp
-void show(Complex *f, unsigned int n); // temp
-void init2(Complex *f, Complex *g, unsigned int m); // temp
+void init(double *f, unsigned int n); /* temp */
+void show(double *f, unsigned int n); /* temp */
+void init2(double *f, double *g, unsigned int m); /* temp */
 
 ImplicitConvolution *fftwpp_create_conv1d(unsigned int m);
 void fftwpp_conv1d_convolve(ImplicitConvolution *conv, double *a, double *b);
 
 ImplicitHConvolution *fftwpp_create_hconv1d(unsigned int m);
 void fftwpp_hconv1d_convolve(ImplicitHConvolution *conv, double *a, double *b);
-
-
-#endif
-
 
 #endif
