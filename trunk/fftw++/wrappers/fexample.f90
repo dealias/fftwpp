09788bac6279352a0954a96581022f26be9a5646
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-10-02 20:26:37 +0000

    more work on fortran wrappers

--- trunk/fftw++/wrappers/fexample.f90
+++ trunk/fftw++/wrappers/fexample.f90
@@ -2,16 +2,35 @@ program fexample
   use fftwpp_module
 
   implicit NONE
-  integer :: m=8
+  integer :: m, i
   complex :: z =(2,3)
-  complex, dimension(8) :: f
-  ! FIXME: make f and g allocatable or whatever it is in Fortran.
+  complex :: cf, cg
+  double complex pf, pg
+  real, pointer :: dp
+  
+  complex, dimension(:), allocatable :: f, g
    
   type(hconv1d_type) :: conv
   write(*,*) z
 
+  m=8
+
+  ! FIXME: need to align memory here
+  allocate(f(m))
+  allocate(g(m))
+
+  do i=0,m-1
+     f(i+1)=cmplx(i,(i+1))
+     g(i+1)=cmplx(i,(2*i +1))
+     print*,f(i+1)
+  end do
+
+
   call new_hconv1d(conv,m)
-  
-  !call del_hconv1d(conv) !segfaults, just like before, yo.
+  call conv_hconv1d(conv,cf,cg) ! FIXME: pass pointers to arrays
+  !call del_hconv1d(conv) !FIXME: segfaults
+
+  deallocate(f)
+  deallocate(g)
 
 end program fexample
