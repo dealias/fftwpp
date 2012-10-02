db92c41e11269eb9d6b212c575bd1c4bf1660baa
miroberts <malcolm.i.w.roberts@gmail.com>
2012-10-02 15:08:00 +0000

    more work on fotran wrappers

--- wrappers/fexample.f90
+++ wrappers/fexample.f90
@@ -4,11 +4,14 @@ program fexample
   implicit NONE
   integer :: m=8
   complex :: z =(2,3)
-
+  complex, dimension(8) :: f
+  ! FIXME: make f and g allocatable or whatever it is in Fortran.
+   
   type(hconv1d_type) :: conv
   write(*,*) z
 
   call new_hconv1d(conv,m)
   
+  !call del_hconv1d(conv) !segfaults, just like before, yo.
 
 end program fexample
