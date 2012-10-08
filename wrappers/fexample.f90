854f93cf205c99c2d881f593a7bb93b21911c30e
miroberts <malcolm.i.w.roberts@gmail.com>
2012-10-08 09:17:46 +0000

    1d fortran wrappers now workingxc

--- wrappers/fexample.f90
+++ wrappers/fexample.f90
@@ -1,55 +1,81 @@
 program fexample
-  !use fftwpp_module
   use fftwpp
 
   use, intrinsic :: ISO_C_Binding !FIXME: use only.... to clean up namespace?
   implicit NONE
-  include 'fftw3.f03' !FIXME: have to link the file to pwd right now. Makefile?
+  include 'fftw3.f03'
   integer(c_int) :: m
   integer :: i
   
-!  type(hconv1d_type) :: conv
   complex(C_DOUBLE_COMPLEX), pointer :: f(:), g(:)
-  type(C_PTR) :: pf, pg, pconv
- 
+  type(C_PTR) :: pf, pg, hconv1d, cconv1d
+  
+  write(*,*) "Example of calling fftw++ convolutions from Fortran:"
+
   m=8 ! problem size
 
-  write(*,*) "allocate memory:"
-  write(*,*) associated(f)
-  pf = fftw_alloc_complex(int(m, C_SIZE_T)) ! allocate 
+  !allocate memory:
+  pf = fftw_alloc_complex(int(m, C_SIZE_T))
   call c_f_pointer(pf, f, [m])
-  pg = fftw_alloc_complex(int(m, C_SIZE_T)) ! allocate 
+  pg = fftw_alloc_complex(int(m, C_SIZE_T))
   call c_f_pointer(pg, g, [m])
-  write(*,*) associated(f)
 
   ! initialize arrays
-  do i=0,m-1
-     f(i+1)=cmplx(i,(i+1))
-     g(i+1)=cmplx(i,(2*i +1))
-     print*,f(i+1)
-  end do
+  call init(f,g,m)
 
   write(*,*)
-  write(*,*) "Create convolution:"
-  pconv=hconv1d_create(m)
+  write(*,*) "input f:"
+  call output(f,m)
 
   write(*,*)
-  write(*,*) "convolve:"
-  call hconv1d_convolve(pconv,pf,pg)
+  write(*,*) "input g:"
+  call output(g,m)
 
-!  write(*,*)
-!  write(*,*) "delete C++ object:"
-!  call delete_hconv1d(pconv)! sefaults
-
-
-  ! output of result
   write(*,*)
-  write(*,*) "output:"
-  do i=1,m
-     print*,f(i)
-  end do
+  write(*,*) "1d non-centered complex convolution:"
+  cconv1d=cconv1d_create(m)   !Create non-centered convolution:
+  call cconv1d_convolve(cconv1d,pf,pg)   !convolve:
+  call delete_cconv1d(cconv1d) !delete C++ object and free work memory
 
-  !deallocate(f) ! needs to be done with fftw_delete or some such thing.
-  !deallocate(g)
+  call output(f,m)
+
+  call init(f,g,m) ! reset input data
+
+  write(*,*)
+  write(*,*) "1d centered Hermitian-symmetric complex convolution:"
+  hconv1d=hconv1d_create(m) !Create Hermitian-symmetric centered convolution:
+  call hconv1d_convolve(hconv1d,pf,pg) ! convolve
+  call delete_hconv1d(hconv1d)   !delete C++ object and free work memory
+  call output(f,m)
+
+  ! memory-aligned arrays need to be deleted using FFTW.
+  call fftw_free(pf)
+  call fftw_free(pg)
+
+  contains
+    
+    subroutine init(f,g,m)
+      use, intrinsic :: ISO_C_Binding
+      implicit NONE
+      integer :: i
+      integer(c_int), intent(in) :: m
+      complex(C_DOUBLE_COMPLEX), pointer, intent(inout) :: f(:), g(:)
+      do i=0,m-1
+         f(i+1)=cmplx(i,(i+1))
+         g(i+1)=cmplx(i,(2*i +1))
+      end do
+    end subroutine init
+    
+    subroutine output(f,m)
+      use, intrinsic :: ISO_C_Binding
+      implicit NONE
+      integer :: i
+      integer(c_int), intent(in) :: m
+      complex(C_DOUBLE_COMPLEX), pointer, intent(inout) :: f(:)
+      do i=1,m
+         print*,f(i)
+      end do
+    end subroutine output
 
 end program fexample
+
--- wrappers/fftwpp.f90
+++ wrappers/fftwpp.f90
@@ -2,7 +2,30 @@ module fftwpp
   use iso_c_binding
 
   interface
-     type(c_ptr) function hconv1d_create(m) bind(c, name='fftwpp_create_hconv1d')
+     type(c_ptr) function cconv1d_create(m) &
+          bind(c, name='fftwpp_create_conv1d')
+       use iso_c_binding
+       integer(c_int), intent(in), value :: m
+     end function cconv1d_create
+  end interface
+
+  interface
+     subroutine cconv1d_convolve(p,f,g) bind(c, name='fftwpp_conv1d_convolve')
+       use iso_c_binding
+       type(c_ptr), intent(in), value :: p,f,g
+     end subroutine cconv1d_convolve
+  end interface
+
+  interface
+     subroutine delete_cconv1d(p) bind(c, name='fftwpp_conv1d_delete')
+       use iso_c_binding
+       type(c_ptr), intent(in), value  :: p
+     end subroutine delete_cconv1d
+  end interface
+
+  interface
+     type(c_ptr) function hconv1d_create(m) &
+          bind(c, name='fftwpp_create_hconv1d')
        use iso_c_binding
        integer(c_int), intent(in), value :: m
      end function hconv1d_create
