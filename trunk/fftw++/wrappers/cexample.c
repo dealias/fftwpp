1bd613467974437660292fdf62c442a9a17281e9
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-10-09 14:49:02 +0000

    Add C wrappers for 2D and 3D binary convolutions, with example.

--- trunk/fftw++/wrappers/cexample.c
+++ trunk/fftw++/wrappers/cexample.c
@@ -2,7 +2,6 @@
 #include "cfftw++.h"
 #include<complex.h>
 
-
 void init(double complex *f, double complex *g, unsigned int m) 
 {
   for(unsigned int k=0; k < m; k++) {
@@ -17,12 +16,65 @@ void show(double complex *f, unsigned int m)
     printf("(%.2f,%.2f)\n", creal(f[k]), cimag(f[k]));
 }
 
+void init2(double complex** F, double complex** G,
+	   unsigned int mx, unsigned int my)
+{
+  for(unsigned int i=0; i < mx; ++i) {
+    for(unsigned int j=0; j < my; j++) {
+      F[i][j]=i+j*I;
+      G[i][j]=2*i+(j+1)*I;
+    }
+  }
+}
+
+void show2(double complex** F, 
+	   unsigned int mx, unsigned int my)
+{
+  for(unsigned int i=0; i < mx; ++i) {
+    for(unsigned int j=0; j < my; j++) {
+      printf("(%.2f,%.2f) ", creal(F[i][j]), cimag(F[i][j]));
+    }
+    printf("\n");
+  }
+}
+
+void init3(double complex *f, double complex *g, 
+	   unsigned int mx, unsigned int my, unsigned int mz)
+{
+  int pos=0;
+  for(unsigned int i=0; i < mx; ++i) {
+    for(unsigned int j=0; j < my; j++) {
+      for(unsigned int k=0; k < mz; k++) {
+	f[pos]=(i+k) +I*(j+k);
+	g[pos]=(2*i+k)+I*(j+1+k);
+	pos++;
+      }
+    }
+  }
+}
+
+void show3(double complex *f, 
+	   unsigned int mx, unsigned int my, unsigned int mz)
+{
+  int pos=0;
+  for(unsigned int i=0; i < mx; ++i) {
+    for(unsigned int j=0; j < my; j++) {
+      for(unsigned int k=0; k < mz; k++) {
+	printf("(%.0f,%.0f) ", creal(f[pos]), cimag(f[pos]));
+	pos++;
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+}
 
 int main()
 {
   printf("Example of calling fftw++ convolutions from C:\n");
   
-  { // 1D examples
+  // 1D examples
+  { 
     unsigned int m=8; /* problem size */
     
     /* input arrays must be aligned */
@@ -49,32 +101,133 @@ int main()
     fftwpp_hconv1d_convolve(conv,f,g);
     fftwpp_hconv1d_delete(conv);
     show(f,m);
+
+    // free memory
+    delete_complexAlign(g);
+    delete_complexAlign(f);
+
   }
 
-  { // 2D examples
-    unsigned int mx=4, my=4;
+  // 2D examples
+  { 
+    printf("\n2d non-centered complex convolution:\n");
+    unsigned int mx=4, my=4;  /* problem size */
     double complex *f=create_complexAlign(mx*my);
     double complex *g=create_complexAlign(mx*my);
 
-    f[0]=1 + 2* I;
-    printf("The value of f is: %p\n", f);
-    printf("%f + i%f\n", creal(f[0]), cimag(f[0]));
-    // FIXME: init f and g
-    double complex* (X)[mx]; // 2D array that points to f, for convenience
-    int i=0;
-    for(i=0; i < mx; ++i) 
-      X[i]=&f[i*mx];
+    // 2D arrays for convenience
+    double complex* (F)[mx];
+    double complex* (G)[mx];
+    int i;
+    for(i=0; i < mx; ++i)  {
+      F[i]=&f[i*my];
+      G[i]=&g[i*my];
+    }
+
+    init2(F,G,mx,my);
+
+    printf("\ninput f:\n");
+    show2(F,mx,my);
+    printf("\ninput g:\n");
+    show2(G,mx,my);
     
-    printf("The value of X is: %p\n", X[0]);
-    printf("%f + i%f\n", creal(X[0][0]), cimag(X[0][0]));
+    ImplicitConvolution2 *cconv=fftwpp_create_conv2d(mx,my);
+    fftwpp_conv2d_convolve(cconv,f,g);
+    fftwpp_conv2d_delete(cconv);
+    printf("\noutput:\n");
+    show2(F,mx,my);
     
-    //ImplicitConvolution2 *cconv=fftwpp_create_conv2d(mx,my);
-    //fftwpp_conv2d_convolve(cconv,f,g);
-    // FIXME: output result
+    delete_complexAlign(g);
+    delete_complexAlign(f);
+  
+  }
 
+  {
+    printf("\n2d centered Hermitian-symmetric convolution:\n");
+    unsigned int mx=4, my=4;  /* problem size */
+    
+    unsigned int Mx=2*mx-1;
+    double complex *f=create_complexAlign(Mx*my);
+    double complex *g=create_complexAlign(Mx*my);
+    
+    // 2D arrays for convenience
+    double complex* (F)[Mx];
+    double complex* (G)[Mx];
+    int i;
+    for(i=0; i < Mx; ++i)  {
+      F[i]=&f[i*my];
+      G[i]=&g[i*my];
+    }
 
-  }  
+    init2(F,G,Mx,my);
+    
+    printf("\ninput f:\n");
+    show2(F,Mx,my);
+    printf("\ninput g:\n");
+    show2(G,Mx,my);
+    
+    ImplicitHConvolution2 *conv=fftwpp_create_hconv2d(mx,my);
+    fftwpp_hconv2d_convolve(conv,f,g);
+    fftwpp_hconv2d_delete(conv);
 
+    printf("\noutput:\n");
+    show2(F,Mx,my);
+
+    delete_complexAlign(g);
+    delete_complexAlign(f);
+  }
+
+  // 3D examples
+  {
+    printf("\n3d non-centered complex convolution:\n");
+    
+    unsigned int mx=4, my=4, mz=4;  /* problem size */
+    unsigned int mxyz=mx*my*mz;
+    double complex *f=create_complexAlign(mxyz);
+    double complex *g=create_complexAlign(mxyz);
+    
+    init3(f,g,mx,my,mz);
+    printf("\ninput f:\n");
+    show3(f,mx,my,mz);
+    printf("\ninput g:\n");
+    show3(g,mx,my,mz);
+    
+    ImplicitConvolution3 *cconv=fftwpp_create_conv3d(mx,my,mz);
+    fftwpp_conv3d_convolve(cconv,f,g); 
+    fftwpp_conv3d_delete(cconv);
+    
+    printf("\noutput:\n");
+    show3(f,mx,my,mz);
+    delete_complexAlign(g);
+    delete_complexAlign(f);
+
+  }
+  {
+    printf("\n3d non-centered complex convolution:\n");
+    
+    unsigned int mx=4, my=4, mz=4;  /* problem size */
+    unsigned int Mx=2*mx-1;
+    unsigned int My=2*my-1;
+
+    unsigned int mxyz=Mx*My*mz;
+    double complex *f=create_complexAlign(mxyz);
+    double complex *g=create_complexAlign(mxyz);
+    
+    init3(f,g,Mx,My,mz);
+    printf("\ninput f:\n");
+    show3(f,Mx,My,mz);
+    printf("\ninput g:\n");
+    show3(g,Mx,My,mz);
+    
+    ImplicitHConvolution3 *conv=fftwpp_create_hconv3d(mx,my,mz);
+    fftwpp_hconv3d_convolve(conv,f,g); 
+    fftwpp_hconv3d_delete(conv);
+    
+    printf("\noutput:\n");
+    show3(f,Mx,My,mz);
+    delete_complexAlign(g);
+    delete_complexAlign(f);
+  }
   return 0;
 }
 
