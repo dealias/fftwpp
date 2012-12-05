a59dd0bfa7843409da9770479dfe77dab817b40b
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-12-05 10:11:09 +0000

    Separate examples for different convolutions.

---
+++ trunk/fftw++/examples/examplecconv.cc
@@ -0,0 +1,59 @@
+#include "Complex.h"
+#include "convolution.h"
+#include "Array.h"
+
+// Compile with:
+// g++ -I .. -fopenmp examplecconv.cc ../convolution.cc ../fftw++.cc -lfftw3 -lfftw3_omp
+
+using namespace std;
+using namespace Array;
+using namespace fftwpp;
+
+// size of problem
+unsigned int m=8;
+
+unsigned int mx=4;
+unsigned int my=4;
+
+inline void init(Complex *f, Complex *g) 
+{
+  unsigned int m1=m+1;
+  for(unsigned int i=0; i < m1; i += m1) {
+    Complex *fi=f+i;
+    Complex *gi=g+i;
+    for(unsigned int k=0; k < m; k++) fi[k]=Complex(k,k+1);
+    for(unsigned int k=0; k < m; k++) gi[k]=Complex(k,2*k+1);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  fftw::maxthreads=omp_get_max_threads();
+
+#ifndef __SSE2__
+  fftw::effort |= FFTW_NO_SIMD;
+#endif  
+  
+   // 1d non-centered complex convolution
+    
+  // allocate arrays:
+  Complex *f=ComplexAlign(m);
+  Complex *g=ComplexAlign(m);
+  
+  cout << endl << "non-centered complex convolution:" << endl;
+  init(f,g);
+  cout << "input:\nf\tg" << endl;
+  for(unsigned int i=0; i < m; i++) 
+    cout << f[i] << "\t" << g[i] << endl;
+  
+  ImplicitConvolution C(m);
+  C.convolve(f,g);
+  
+  cout << "\noutput:" << endl;
+  for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
+    
+  deleteAlign(g);
+  deleteAlign(f);
+
+  return 0;
+}
---
+++ trunk/fftw++/examples/exampleconv3.cc
@@ -0,0 +1,70 @@
+#include "Complex.h"
+#include "convolution.h"
+#include "Array.h"
+
+// Compile with:
+// g++ -I .. -fopenmp exampleconv3.cc ../convolution.cc ../fftw++.cc -lfftw3 -lfftw3_omp
+
+using namespace std;
+using namespace Array;
+using namespace fftwpp;
+
+// size of problem
+unsigned int mx=4;
+unsigned int my=4;
+unsigned int mz=4;
+
+inline void init(array3<Complex>& f, array3<Complex>& g, unsigned int M=1) 
+{
+  unsigned int xstop=2*mx-1;
+  unsigned int ystop=2*my-1;
+  double factor=1.0/sqrt((double) M);
+  for(unsigned int s=0; s < M; ++s) {
+    double S=sqrt(1.0+s);
+    double ffactor=S*factor;
+    double gfactor=1.0/S*factor;
+    for(unsigned int i=0; i < xstop; ++i) {
+      unsigned int I=s*xstop+i;
+      for(unsigned int j=0; j < ystop; ++j) {
+        for(unsigned int k=0; k < mz; ++k) {
+          f[I][j][k]=ffactor*Complex(i+k,j+k);
+          g[I][j][k]=gfactor*Complex(2*i+k,j+1+k);
+        }
+      }
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  fftw::maxthreads=omp_get_max_threads();
+
+#ifndef __SSE2__
+  fftw::effort |= FFTW_NO_SIMD;
+#endif  
+  
+
+  // 2d Hermitian-symmetric centered convolution
+  cout << endl << "3D centered Hermitian-symmetric convolution:" << endl;
+
+  size_t align=sizeof(Complex);
+  array3<Complex> f(2*mx-1,2*my-1,mz,align);
+  array3<Complex> g(2*mx-1,2*my-1,mz,align);
+
+  init(f,g);
+  cout << "\ninput:" << endl;
+  cout << "f:" << endl << f << endl;
+  cout << "g:" << endl << g << endl;
+  
+  cout << "input after symmetrization (done automatically):" << endl;
+  HermitianSymmetrizeXY(mx,my,mz,f);
+  HermitianSymmetrizeXY(mx,my,mz,g);
+  cout << "f:" << endl << f << endl;
+  cout << "g:" << endl << g << endl;
+  
+  ImplicitHConvolution3 C(mx,my,mz);
+  C.convolve(f,g);
+  cout << "output:" << endl << f << endl;
+
+  return 0;
+}
