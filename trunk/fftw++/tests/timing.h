abb93d1ff6699bbcce686cdde702afb704a4e95d
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-12-18 22:28:15 +0000

    Add test-case of 2D mpi convolition using FFTW, split utils.h into timing.h

---
+++ trunk/fftw++/tests/timing.h
@@ -0,0 +1,53 @@
+#ifndef __timing_h__
+#define __timing_h__ 1
+
+#include "math.h"
+
+inline double emptytime(double *T, unsigned int N)
+{
+  double val=0.0;
+  for(unsigned int i=0; i < N; ++i) {
+    seconds();
+    T[i]=seconds();
+  }
+  for(unsigned int i=0; i < N; ++i) 
+    val += T[i];
+  return val/N;
+}
+
+inline double mean(double *T, unsigned int N) 
+{
+  double sum=0.0;
+  for(unsigned int i=0; i < N; ++i)
+    sum += T[i];
+  return sum/N;
+}
+
+inline void stdev(double *T, unsigned int N, double mean, double &sigmaL,
+           double& sigmaH) 
+{
+  sigmaL=0.0, sigmaH=0.0;
+  for(unsigned int i=0; i < N; ++i) {
+    double v=T[i]-mean;
+    if(v < 0)
+      sigmaL += v*v;
+    if(v > 0)
+      sigmaH += v*v;
+  }
+  
+  double factor=N > 2 ? 2.0/(N-2.0) : 0.0; 
+  sigmaL=sqrt(sigmaL*factor);
+  sigmaH=sqrt(sigmaH*factor);
+}
+
+inline void timings(const char* text, double *T, unsigned int N)
+{
+  double sigmaL=0.0, sigmaH=0.0;
+  double avg=mean(T,N);
+  stdev(T,N,avg,sigmaL,sigmaH);
+  avg -= emptytime(T,N);
+  std::cout << std::endl << text << ":\n" << avg << "\t" << sigmaL << "\t" <<
+    sigmaH << std::endl << std::endl;
+}
+
+#endif
