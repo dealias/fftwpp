47df7dd37a4e2c5ede34efeaf630579b8fb606a0
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2012-12-29 23:14:33 +0000

    Add 3D mpi explicitly padded non-centered complex convolution.

--- trunk/fftw++/mpi/fftw/cconv3.cc
+++ trunk/fftw++/mpi/fftw/cconv3.cc
@@ -9,18 +9,22 @@
 using namespace std;
 
 // compile with
-// mpicxx -o cconv2 cconv2.cc -lfftw3_mpi -lfftw3 -lm
+// mpicxx -o cconv3 cconv3.cc -lfftw3_mpi -lfftw3 -lm
 
 void show(fftw_complex *f, int local_0_start, int local_n0, 
-	  int N1, int m0, int m1, int A)
+	  int N1, int N2, int m0, int m1, int m2, int A)
 {
   int stop=local_0_start+local_n0;
   for (int i = local_0_start; i < stop; ++i) {
     if(i < m0) {
       int ii=i-local_0_start;
-      cout << A << i << ": ";
       for (int j = 0; j < m1; ++j) {
-	cout << "("<<creal(f[ii*N1 + j])<<","<<cimag(f[ii*N1 + j]) << ")  ";
+	cout << A << "-" << i << ": ";
+	for (int k = 0; k < m2; ++k) {
+	  int index=ii*N1*N2 + j*N1 +k;
+	  cout << "("<<creal(f[index])<<","<<cimag(f[index]) << ")  ";
+	}
+	cout << endl;
       }
       cout << endl;
     }
@@ -28,22 +32,34 @@ void show(fftw_complex *f, int local_0_start, int local_n0,
 } 
 
 void init(fftw_complex *f, fftw_complex *g, int local_0_start, int local_n0, 
-	  int N0, int N1, int m0, int m1)
+	  int N0, int N1, int N2, int m0, int m1, int m2)
 {
   int stop=local_0_start+local_n0;
   for (int i = local_0_start; i < stop; ++i) {
     if(i < m0) {
       int ii=i-local_0_start;
       for (int j = 0; j < m1; ++j) {
-	f[ii*N1+j]=i +j*I;
-	g[ii*N1+j]=2*i +(j+1)*I;
-
-	// f[ii*N1+j]=i*I;
-	// g[ii*N1+j]=(i == 0 && j == 0) ? 1.0 : 0.0; //i*I;
+	for (int k = 0; k < m2; ++k) {
+	  int index=ii*N1*N2 + j*N1 +k;
+	  f[index]=i+k +(j+k)*I;
+	  g[index]=2*i+k +(j+1+k)*I;
+	  
+	  // f[ii*N1+j]=i*I;
+	  // g[ii*N1+j]=(i == 0 && j == 0) ? 1.0 : 0.0; //i*I;
+	}
+	for (int k = m2; k < N2; ++k) {
+	  int index=ii*N1*N2 + j*N1 +k;
+	  f[index]=0.0;
+	  g[index]=0.0;
+	}
+	
       }
       for (int j = m1; j < N1; ++j) {
-	f[ii*N1+j]=0.0;
-	g[ii*N1+j]=0.0;
+	for (int k = 0; k < N2; ++k) {
+	  int index=ii*N1*N2 + j*N1 +k;
+	  f[index]=0.0;
+	  g[index]=0.0;
+	}
       }
     }
   }
@@ -51,8 +67,11 @@ void init(fftw_complex *f, fftw_complex *g, int local_0_start, int local_n0,
   for (int i = 0; i < local_n0; ++i) {
     if(i+local_0_start >= m0) {
       for (int j = 0; j < N1; ++j) {
-	f[i*N1+j]=0.0;
-	g[i*N1+j]=0.0;
+	for (int k = 0; k < N2; ++k) {
+	  int index=i*N1*N2 + j*N1 +k;
+	  f[index]=0.0;
+	  g[index]=0.0;
+	}
       }
     }
   }
@@ -92,8 +111,8 @@ int main(int argc, char **argv)
     }
   }
 
-  const unsigned int m0 = m, m1 = m;
-  const unsigned int N0 = 2*m0, N1 = 2*m1;
+  const unsigned int m0 = m, m1 = m, m2=m;
+  const unsigned int N0 = 2*m0, N1 = 2*m1, N2 = 2*m2;
   fftw_plan fplan, iplan;
   fftw_complex *f, *g;
   ptrdiff_t alloc_local, local_n0, local_0_start;
@@ -105,36 +124,33 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   
   /* get local data size and allocate */
-  alloc_local = fftw_mpi_local_size_2d(N0,N1,MPI_COMM_WORLD,
+  alloc_local = fftw_mpi_local_size_3d(N0,N1,N2,MPI_COMM_WORLD,
 				       &local_n0, &local_0_start);
+  
+  ptrdiff_t local_n1, local_1_start;
+  int transize=fftw_mpi_local_size_3d_transposed(N0,N1,N2,MPI_COMM_WORLD,
+						 &local_n0,&local_0_start,
+						 &local_n1,&local_1_start);
   f=fftw_alloc_complex(alloc_local);
   g=fftw_alloc_complex(alloc_local);
   
   /* create plan for in-place DFT */
-  fplan=fftw_mpi_plan_dft_2d(N0,N1,f,f,MPI_COMM_WORLD,FFTW_FORWARD,
+  fplan=fftw_mpi_plan_dft_3d(N0,N1,N2,f,f,MPI_COMM_WORLD,FFTW_FORWARD,
 			     FFTW_ESTIMATE | FFTW_MPI_TRANSPOSED_IN);
-  iplan=fftw_mpi_plan_dft_2d(N0,N1,f,f,MPI_COMM_WORLD,FFTW_BACKWARD,
+  iplan=fftw_mpi_plan_dft_3d(N0,N1,N2,f,f,MPI_COMM_WORLD,FFTW_BACKWARD,
 			     FFTW_ESTIMATE | FFTW_MPI_TRANSPOSED_OUT);
 
-  
-  init(f,g,local_0_start,local_n0,N0,N1,m0,m1);
+  init(f,g,local_0_start,local_n0,N0,N1,N2,m0,m1,m2);
 
-  // show(f,local_0_start,local_n0,N1,m0,m1,0);
-  // show(f,local_0_start,local_n0,N1,m0,m1,0);
-  //show(g,local_0_start,local_n0,N1,N0,N1,1);
 
-  // determine number of elements per process after tranpose
-  ptrdiff_t local_n1, local_1_start;
-  unsigned int transize=
-    fftw_mpi_local_size_2d_transposed(N0,N1,MPI_COMM_WORLD,
-   				      &local_n0, &local_0_start,
-   				      &local_n1, &local_1_start);
-  
+  show(f,local_0_start,local_n0,N1,N2,m0,m1,m2,0);
+  //show(f,local_0_start,local_n0,N1,N2,N0,N1,N2,1);
+  show(g,local_0_start,local_n0,N1,N2,m0,m1,m2,1);
+
   double *T=new double[N];
-
   for(int i=0; i < N; ++i) {
-    init(f,g,local_0_start,local_n0,N0,N1,m0,m1);
-    double overN=1.0/((double) (N0*N1));
+    init(f,g,local_0_start,local_n0,N0,N1,N2,m0,m1,m2);
+    double overN=1.0/((double) (N0*N1*N2));
     seconds();
     convolve(f,g,overN,transize,fplan,iplan);
     T[i]=seconds();
@@ -142,10 +158,12 @@ int main(int argc, char **argv)
 
   if(rank == 0) timings("Explicit",m,T,N);
   
-  if(m0*m1<100) {
+  if(m0*m1*m2<100) {
     if(rank == 0) cout << "output:" << endl;
-    show(f,local_0_start,local_n0,N1,m0,m1,2);
+    show(f,local_0_start,local_n0,N1,N2,m0,m1,m2,2);
   }
+
+
   fftw_destroy_plan(fplan);
   fftw_destroy_plan(iplan);
 
