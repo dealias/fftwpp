c6ff4fbd0f732330172cd5f79ef5549eed0e058f
miroberts <malcolm.i.w.roberts@gmail.com>
2013-03-06 14:00:12 +0000

    reduce problem size in alltime

--- mpi/ignorance.sh
+++ mpi/ignorance.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# remove lines relevant to FFTW's MPI transpose routine to work around
+# a bug in FFTW's transpose routine causing segfaults when tranposing
+# arrays with two points in at least one dimension.
+
 sed -i '/fftw_mpi_transpose/d' wisdom3.txt 
