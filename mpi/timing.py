ca0615e97ab754fd961bb8d34dc91f998e3932ef
miroberts <malcolm.i.w.roberts@gmail.com>
2013-04-01 20:16:51 +0000

    Allow maximum memory use specification in timing.py

--- mpi/timing.py
+++ mpi/timing.py
@@ -8,7 +8,7 @@ from math import *
 import os
 
 def main(argv):
-    usage='Usage: timings.py -a<start> -b<stop> -p<cconv2,conv2,cconv3,conv3> -T<number of threads per node> -P<number of nodes> -A<quoted arg list for timed program> -M <quoted arg list for mpi run command> -r<implicit/explicit> -l<name of mpi run command>' 
+    usage='Usage: timings.py -a<start> -b<stop> -p<cconv2,conv2,cconv3,conv3> -T<number of threads per node> -P<number of nodes> -A<quoted arg list for timed program> -M <quoted arg list for mpi run command> -r<implicit/explicit> -l<name of mpi run command> -R<ram in gigabytes>' 
     
     Tset=0
     P=1
@@ -21,8 +21,9 @@ def main(argv):
     b=8
     r="implicit"
     l="mpiexec"
+    RAM=0
     try:
-        opts, args = getopt.getopt(argv,"p:T:P:a:b:A:M:r:l:")
+        opts, args = getopt.getopt(argv,"p:T:P:a:b:A:M:r:l:R:")
     except getopt.GetoptError:
         print usage
         sys.exit(2)
@@ -47,7 +48,8 @@ def main(argv):
             r=str(arg)
         elif opt in ("-l"):
             l=str(arg)
-
+        elif opt in ("-R"):
+            RAM=float(arg)*2**30
 
     if p == "":
         print "please specify a program with -p"
@@ -57,13 +59,27 @@ def main(argv):
     outdir=""
    
     if p == "cconv2":
+        if RAM != 0:
+            #b=int(floor(log(RAM/16/2/2^2)/log(2)/2))
+            b=int(floor(0.5*log(RAM/64)/log(2)))
         outdir="timings2c"
     if p == "conv2":
+        if RAM != 0:
+            #print int(floor(log(RAM/8/3^2)/log(2)/2))
+            b= int(floor(0.5*log(RAM/96)/log(2)))
         outdir="timings2r"
     if p == "cconv3":
+        if RAM != 0:
+           #b=int(floor(log(RAM/16/2^3)/log(2)/3))
+           b=int(floor(log(RAM/96)/log(2)/3))
         outdir="timings3c"
     if p == "conv3":
+        if RAM != 0:
+            b=int(floor(log(RAM/192)/log(2)/3))
         outdir="timings3r"
+
+    if RAM != 0:
+        print "max problem size is "+str(2**b)
 
     if outdir == "":
         print "empty outdir: please select a different program!"
@@ -72,7 +88,6 @@ def main(argv):
         sys.exit(2)
 
     outdir=outdir+"/"+str(P)+"x"+str(T)
-#    command=l+" "+M+" --np "+str(P)+" : ./"+str(p)
     command=l+"  --np "+str(P)+" "+M+"  ./"+str(p)
 
     print "output in "+outdir+"/"+r
@@ -89,7 +104,7 @@ def main(argv):
             print "cannot use multiple threads with explicit: try without -T"
             sys.exit(2)
 
-    for i in range(a,b):
+    for i in range(a,b+1):
         print i,
         sys.stdout.flush()
         run=command+cargs+" -m "+str(int(pow(2,i)))+A
@@ -102,6 +117,7 @@ def main(argv):
         #os.system(run+grepc+" "+cat)
         #print run
         #os.system(run)
+        sys.stdout.flush()
 
     os.system("sed -i 's/[ \t]*$//' "+outdir+"/"+r)
     os.system("sed -i '/^$/d' "+outdir+"/"+r)
