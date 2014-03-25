aa4bcf8bcc8abfe1bd288ee41121e30fc473b447
jcbowman <jcbowman@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2014-03-25 20:57:55 +0000

    Fix multiplication bug in show.

--- trunk/fftw++/mpi/mpiutils.h
+++ trunk/fftw++/mpi/mpiutils.h
@@ -3,6 +3,18 @@
 
 namespace fftwpp {
 
+template<class ftype>
+void show(ftype *f, unsigned int nx, unsigned int ny)
+{
+  unsigned int c=0;
+  for(unsigned int i=0; i < nx; ++i) {
+    for(unsigned int j=0; j < ny; ++j) {
+      std::cout << f[c++]  << "\t";
+    }
+    std::cout << std::endl;
+  }
+}
+
 // output the contents of a 2D array
 template<class ftype>
 void show(ftype *f, unsigned int nx, unsigned int ny,
@@ -15,46 +27,44 @@ void show(ftype *f, unsigned int nx, unsigned int ny,
   
   if(rank == 0) {
     std::cout << "process " << 0 << ":" <<  std::endl;
-    unsigned int c=0;
-    for(unsigned int i=0; i < nx; i++) {
-      for(unsigned int j=0; j < ny; j++) {
-        std::cout << f[c++] << "\t";
-      }
-      std::cout << std::endl;
-    }
+    show(f,nx,ny);
     
     for(int p=1; p < size; p++) {
-      int source=p, tag=p;
-      unsigned int pdims[2];
-      MPI_Recv(&pdims,2,MPI_UNSIGNED,source,tag,communicator,&stat);
+      unsigned int dims[2];
+      MPI_Recv(&dims,2,MPI_UNSIGNED,p,0,communicator,&stat);
 
-      unsigned int px=pdims[0], py=pdims[1];
+      unsigned int px=dims[0], py=dims[1];
       unsigned int n=px*py;
       ftype *C=new ftype[n];
-      tag += size;
-      MPI_Recv(C,sizeof(ftype)*n,MPI_BYTE,source,tag,communicator,&stat);
+      MPI_Recv(C,sizeof(ftype)*n,MPI_BYTE,p,0,communicator,&stat);
       
       std::cout << "process " << p << ":" <<  std::endl;
-      unsigned int c=0;
-      for(unsigned int i=0; i < px; i++) {
-        for(unsigned int j=0; j < py; j++) {
-          std::cout << C[c++] << "\t";
-        }
-        std::cout << std::endl;
-      }
+      show(C,px,py);
       delete [] C;
     }
   } else {
-    int dest=0, tag=rank;
     unsigned int dims[]={nx,ny};
-    MPI_Send(&dims,2,MPI_UNSIGNED,dest,tag,communicator);
-    int n=nx*ny;
-    tag += size;
-    MPI_Send(f,sizeof(ftype)*n,MPI_BYTE,dest,tag,communicator);
+    MPI_Send(&dims,2,MPI_UNSIGNED,0,0,communicator);
+    MPI_Send(f,nx*ny*sizeof(ftype),MPI_BYTE,0,0,communicator);
   }
 }
   
-// output the contents of a 3D complex array
+template<class ftype>
+void show(ftype *f, unsigned int nx, unsigned int ny, unsigned int nz)
+{
+  unsigned int c=0;
+  for(unsigned int i=0; i < nx; ++i) {
+    for(unsigned int j=0; j < ny; ++j) {
+      for(unsigned int k=0; k < nz; ++k) {
+        std::cout << f[c++]  << "\t";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+}
+
+// output the contents of a 3D array
 template<class ftype>
 void show(ftype *f, unsigned int nx, unsigned int ny, unsigned int nz,
           const MPI_Comm& communicator)
@@ -64,50 +74,27 @@ void show(ftype *f, unsigned int nx, unsigned int ny, unsigned int nz,
   MPI_Comm_size(communicator,&size);
   MPI_Comm_rank(communicator,&rank);
   
-  if(rank ==0) {
+  if(rank == 0) {
     std::cout << "process " << 0 << ":" <<  std::endl;
-    unsigned c=0;
-    for(unsigned int i=0; i < nx; i++) {
-      for(unsigned int j=0; j < ny; j++) {
-        for(unsigned int k=0; k < nz; k++) {
-          std::cout << f[c++] << "\t";
-        }
-        std::cout << std::endl;
-      }
-      std::cout << std::endl;
-    }
+    show(f,nx,ny,nz);
     
     for(int p=1; p < size; p++) {
-      int source=p, tag=p;
-      unsigned int pdims[3];
-      MPI_Recv(&pdims,3,MPI_UNSIGNED,source,tag,communicator,&stat);
+      unsigned int dims[3];
+      MPI_Recv(&dims,3,MPI_UNSIGNED,p,0,communicator,&stat);
 
-      unsigned int px=pdims[0], py=pdims[1], pz=pdims[2];
-      int n=px*pz*pz;
+      unsigned int px=dims[0], py=dims[1], pz=dims[2];
+      int n=px*py*pz;
       ftype *C=new ftype[n];
-      tag += size;
-      MPI_Recv(C,n*sizeof(ftype),MPI_BYTE,source,tag,communicator,&stat);
+      MPI_Recv(C,n*sizeof(ftype),MPI_BYTE,p,0,communicator,&stat);
       
       std::cout << "process " << p << ":" <<  std::endl;
-      unsigned int c=0;
-      for(unsigned int i=0; i < px; i++) {
-        for(unsigned int j=0; j < py; j++) {
-          for(unsigned int k=0; k < pz; k++) {
-            std::cout << C[c++]  << "\t";
-          }
-          std::cout << std::endl;
-        }
-        std::cout << std::endl;
-      }
+      show(C,px,py,pz);
       delete [] C;
     }
   } else {
-    int dest=0, tag=rank;
     unsigned int dims[]={nx,ny,nz};
-    MPI_Send(&dims,3,MPI_UNSIGNED,dest,tag,communicator);
-    int n=nx*ny*nz;
-    tag += size;
-    MPI_Send(f,n*sizeof(ftype),MPI_BYTE,dest,tag,communicator);
+    MPI_Send(&dims,3,MPI_UNSIGNED,0,0,communicator);
+    MPI_Send(f,nx*ny*nz*sizeof(ftype),MPI_BYTE,0,0,communicator);
   }
 }
 
