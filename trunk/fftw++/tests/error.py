ae8e2e375d0dd06d740873a1bad65b792745316e
miroberts <miroberts@b144c5d6-e4e0-45e3-92d0-d9a5799c5f4d>
2014-04-09 11:54:13 +0000

    Add another case for unittesting

---
+++ trunk/fftw++/tests/error.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python -u
+
+# Error comparison for a variety of implicit and explicit convolutions
+# as compared to a known solution convolution.  
+
+import sys # so that we can return a value at the end.
+from subprocess import * # 
+import re # regexp package
+import random
+
+retval=0
+
+print "Comparison of routine versus test case:"
+print
+print "program\ttype    \tlength\t\terror:"
+print
+
+mlist=[1,2,8,9,1024,random.randint(10,2048)] # problem sizes used in test.
+ilist=["cconv", "conv"]
+for prog in ilist:
+    for mval in mlist:
+        p=Popen(["./"+prog,"-N1","-i","-t","-m"+str(mval)],stdout=PIPE,stderr=PIPE)
+        out, err = p.communicate() # capture output
+        if (p.returncode == 0): # did the process succeed?
+            m=re.search("(?<=error=)(.*)",out) # find the text after "error="
+            print prog +"\t"+"implicit\t"+str(mval)+"\t"+m.group(0)
+            if(float(m.group(0)) > 1e-10):
+                print "ERROR TOO LARGE"
+                retval+=1
+        else:
+            print prog+" FAILED"
+            retval+=1
+
+elist=["cconv","conv"] 
+for prog in elist:
+    for mval in mlist:
+        p=Popen(["./"+prog,"-N1","-e","-t"],stdout=PIPE,stderr=PIPE)
+        out, err = p.communicate() # capture output
+        if (p.returncode == 0): # did the process succeed?
+            m=re.search("(?<=error=)(.*)",out) # find the text after "error="
+            print prog +"\t"+"explicit\t"+str(mval)+"\t"+m.group(0)
+            if(float(m.group(0)) > 1e-10):
+                print "ERROR TOO LARGE"
+                retval+=1
+        else:
+            print prog+" FAILED"
+            retval+=1
+
+print
+
+if(retval == 0):
+    print "OK\tall tests passed"
+else:
+    print "ERROR\tAT LEAST ONE TEST FAILED"
+
+sys.exit(retval)
