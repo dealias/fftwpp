3059687c75ad826513283eb0a02d4d371d2cad5a
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2015-09-05 20:35:45 +0200

    Add unit-test back to mpi/transpose. Kill processes that take more than 1s in mpi/testtranspose.py

--- mpi/testtranspose.py
+++ mpi/testtranspose.py
@@ -4,14 +4,33 @@ import sys # so that we can return a value at the end.
 from subprocess import * # so that we can run commands
 import random # for randum number generators
 
+import time
 import os.path
 
 retval = 0
+
+def waitandkill(proc, timeout):
+    # Check every dt seconds to see if it's done.
+    dt = timeout / 1000.0
+    t = 0
+    while t < timeout:
+        time.sleep(dt)
+        retcode = proc.poll()
+        if(retcode != None):
+            return 0
+        t += dt
+    retcode = proc.poll()
+    if(retcode == None):
+        proc.kill()
+        return 1
+    return 0
 
 if not os.path.isfile("transpose"):
     print "Error: transpose executable not present!"
     retval += 1
 else:
+
+    log = open('testtranspose.log', 'w')
     
     Xlist = [1,2,3,4,5,random.randint(6,128)]
     Ylist = [1,2,3,4,5,random.randint(6,128)]
@@ -20,7 +39,7 @@ else:
         for Y in Ylist:
             for P in Plist:
                 cmd = []
-                cmd.append("mpirun.mpich")
+                cmd.append("mpirun")
                 cmd.append("-n")
                 cmd.append(str(P))
                 cmd.append("./transpose")
@@ -28,18 +47,31 @@ else:
                 cmd.append("-X" + str(X))
                 cmd.append("-Y" + str(Y))
                 print " ".join(cmd)
+                log.write(" ".join(cmd) + '\n')
 
                 proc = Popen(cmd, stdout = PIPE, stderr = PIPE)
+                timeout = 1
+                if(waitandkill(proc, timeout)):
+                    msg = "\tProcess killed after" + str(timeout) +" second(s)!"
+                    print msg
+                    log.write(msg + "\n")
                 proc.wait() # sets the return code
+                
                 prc = proc.returncode
                 out, err = proc.communicate() # capture output
                 if (prc == 0): # did the process succeed?
-                    print "\tSuccess"
+                    msg = "\tSuccess"
+                    print msg
+                    log.write(msg + "\n")
                 else:
-                    print out
-                    print err
-                    print "\t" + " ".join(cmd)
-                    print "\tFAILED!"
-                    retval += 1
+                    msg = "\tFAILED!"
+                    print msg
+                    log.write(msg + "\n")
+                    log.write("stdout:\n" + out + "\n")
+                    log.write("stderr:\n" + err + "\n")
+                    #print out
+                    #print err
 
+                    retval += 1
+    log.close()
 sys.exit(retval)
--- mpi/transpose.cc
+++ mpi/transpose.cc
@@ -22,7 +22,7 @@ void MPISaveWisdom(const MPI_Comm& active);
 }
 
 void init(Complex *data, unsigned int X, unsigned int y, unsigned int Z,
-  ptrdiff_t ystart) {
+	  ptrdiff_t ystart) {
   for(unsigned int i=0; i < X; ++i) { 
     for(unsigned int j=0; j < y; ++j) {
       for(unsigned int k=0; k < Z; ++k) {
@@ -58,8 +58,8 @@ void fftwTranspose(int rank, int size)
   unsigned int block=ceilquotient(Y,size);
   ptrdiff_t alloc=
     fftw_mpi_local_size_many_transposed(2,NN,Z,block,0,
-                                                      MPI_COMM_WORLD,&y,
-                                                      &ystart,&x,&xstart);
+					MPI_COMM_WORLD,&y,
+					&ystart,&x,&xstart);
   if(N == 0) {
     N=N0/(X*y);
     if(N < 10) N=10;
@@ -83,7 +83,7 @@ void fftwTranspose(int rank, int size)
   fftw_plan inplan=fftw_mpi_plan_many_transpose(Y,X,2*Z,block,0,
                                                 (double*) data,(double*) data,
                                                 MPI_COMM_WORLD,
-                                                 FFTW_MPI_TRANSPOSED_IN);
+						FFTW_MPI_TRANSPOSED_IN);
   fftw_plan outplan=fftw_mpi_plan_many_transpose(X,Y,2*Z,0,block,
                                                  (double*) data,(double*) data,
                                                  MPI_COMM_WORLD,
@@ -158,8 +158,10 @@ void fftwTranspose(int rank, int size)
   fftw_destroy_plan(outplan);
 }
   
-void transpose(int rank, int size)
+int transpose(int rank, int size, int N)
 {
+  int retval=0; // success!
+
   Complex *data;
   
   int xsize=localsize(X,size);
@@ -168,131 +170,207 @@ void transpose(int rank, int size)
   
   cout << "Size=" << size << endl;
   
-  int x=localdimension(X,rank,size);
-  int y=localdimension(Y,rank,size);
+  int x = localdimension(X,rank,size);
+  int y = localdimension(Y,rank,size);
   
-//  int xstart=localstart(X,rank,size);
+  int xstart=localstart(X,rank,size);
   int ystart=localstart(Y,rank,size);
   
   MPI_Comm active; 
   MPI_Comm_split(MPI_COMM_WORLD,rank < size,0,&active);
 
-  
   if(rank < size) {
+    cout << "rank=" << rank << " size=" << size << " x=" << x << " " <<
+      " y=" << y << endl;
   
-  cout << "rank=" << rank << " size=" << size << " x=" << x << " " <<
-    " y=" << y << endl;
-  
-  if(N == 0) {
-    N=N0/(X*y);
-    if(N < 10) N=10;
-  }
-  
-  if(rank == 0) {
-    cout << "x=" << x << endl;
-    cout << "y=" << y << endl;
-    cout << "X=" << X << endl;
-    cout << "Y=" << Y << endl;
-    cout << "Z=" << Z << endl;
-    cout << "N=" << N << endl;
-    cout << endl;
-  }
-  
-  data=ComplexAlign(X*y*Z);
-  
-  init(data,X,y,Z,ystart);
-
-//  show(data,X,y*Z,active);
-    
-  // Initialize remaining plans.
-
-  mpitranspose<Complex> T(X,y,x,Y,Z,data,NULL,fftw::maxthreads,active);
-  init(data,X,y,Z,ystart);
-  T.transpose(data,false,true);
-  
-//  show(data,x,Y*Z,active);
-  
-  T.NmTranspose();
-  init(data,X,y,Z,ystart);
-  
-  bool showoutput=X*Y < showlimit && N == 1;
-  if(showoutput)
-    show(data,X,y*Z,active);
-  
-  fftw::statistics Sininit,Sinwait0,Sinwait1,Sin,Soutinit,Soutwait0,Soutwait1,Sout;
-
-  for(int k=0; k < N; ++k) {
-    double begin=0.0, Tinit0=0.0, Tinit=0.0, Twait0=0.0, Twait1=0.0;
-    if(rank == 0) begin=totalseconds();
-
-    T.inphase0();
-    if(rank == 0) Tinit0=totalseconds();
-    T.insync0();
-    if(rank == 0) Twait0=totalseconds();
-    T.inphase1();
-    if(rank == 0) Tinit=totalseconds();
-    T.insync1();
-    if(rank == 0) Twait1=totalseconds();
-    T.inpost();
-
     if(rank == 0) {
-      Sin.add(totalseconds()-begin);
-      Sininit.add(Tinit0-begin);
-      Sinwait0.add(Twait0-Tinit0);
-      Sinwait1.add(Twait1-Tinit);
+      cout << "x=" << x << endl;
+      cout << "y=" << y << endl;
+      cout << "X=" << X << endl;
+      cout << "Y=" << Y << endl;
+      cout << "Z=" << Z << endl;
+      cout << "N=" << N << endl;
+      cout << endl;
     }
-
-    if(showoutput) {
-      if(rank == 0) cout << "\ntranspose:\n" << endl;
-      show(data,x,Y*Z,active);
-    }
-
-    if(rank == 0) begin=totalseconds();
-    T.outphase0();
-    if(rank == 0) Tinit0=totalseconds();
-    T.outsync0();
-    if(rank == 0) Twait0=totalseconds();
-    T.outphase1();
-    if(rank == 0) Tinit=totalseconds();
-    T.outsync1();
-    if(rank == 0) Twait1=totalseconds();
-    if(outtranspose) T.NmTranspose();
-    
-    if(rank == 0) {
-      Sout.add(totalseconds()-begin);
-      Soutinit.add(Tinit0-begin);
-      Soutwait0.add(Twait0-Tinit0);
-      Soutwait1.add(Twait1-Tinit);
-    }
-  }
   
-  if(showoutput) {
-    if(outtranspose) {
-      if(rank == 0) cout << "\nout:\n" << endl;
-      show(data,y,X*Z,active);
-    } else {
-      if(rank == 0) cout << "\noriginal:\n" << endl;
+    data=ComplexAlign(X*y*Z);
+  
+    init(data,X,y,Z,ystart);
+
+    //  show(data,X,y*Z,active);
+    
+    // Initialize remaining plans.
+
+    mpitranspose<Complex> T(X,y,x,Y,Z,data,NULL,fftw::maxthreads,active);
+    init(data,X,y,Z,ystart);
+    T.transpose(data,false,true);
+  
+    //  show(data,x,Y*Z,active);
+  
+    T.NmTranspose();
+    init(data,X,y,Z,ystart);
+  
+    bool showoutput=X*Y < showlimit && N == 1;
+    if(showoutput)
       show(data,X,y*Z,active);
-    }
-  }
+  
+    fftw::statistics Sininit,Sinwait0,Sinwait1,Sin,Soutinit;
+    fftw::statistics Soutwait0,Soutwait1,Sout;
 
-  if(rank == 0) {
-    Sininit.output("Tininit",X);
-    Sinwait0.output("Tinwait0",X);
-    Sinwait1.output("Tinwait1",X);
-    Sin.output("Tin",X);
-    cout << endl;
-    Soutinit.output("Toutinit",X);
-    Soutwait0.output("Toutwait0",X);
-    Soutwait1.output("Toutwait1",X);
-    Sout.output("Tout",X);
-  }
+    if(N == 0) {
+      if(rank == 0)
+	cout << "Diagnostics and unit test.\n" << endl;
+      bool showoutput=true; //X*Y < showlimit;
+
+      init(data,X,y,Z,ystart);
+      if(showoutput) {
+	if(rank == 0) 
+	  cout << "Input:\n" << endl;
+	show(data,X,y*Z,active);
+      }
+
+      Complex *wholedata=NULL, *wholeoutput=NULL;
+      Transpose *localtranspose=NULL;
+      if(rank == 0) {
+	wholedata=new Complex[X*Y*Z];
+	wholeoutput=new Complex[X*Y*Z];
+	localtranspose=new Transpose(X,Y,Z,wholedata);
+      }
+      accumulate_splitx(data,wholedata,X,Y,xstart,ystart,x,y,true,active);
+
+      if(showoutput && rank == 0) {
+	cout << "\naccumulated input data:" << endl;
+	show(wholedata,X,Y,0,0,X,Y);
+      }
+
+
+      T.transpose(data,false,true); // false,true :  N x m -> n x M
+
+      if(showoutput) {
+	if(rank == 0)
+	  cout << "\noutput:\n" << endl;
+	if(outtranspose)
+	  show(data,y,X*Z,active);
+	else
+	  show(data,X,y*Z,active);
+      }
+
+      accumulate_splitx(data,wholeoutput,X,Y,xstart,ystart,x,y,false,active);
+
+      if(rank == 0) {
+	if(outtranspose) 
+	  localtranspose->transpose(wholedata);
+
+	if(showoutput) {
+	  cout << "\naccumulated output data:" << endl;
+	  show(wholeoutput,X,Y,0,0,X,Y);
+	  if(outtranspose) {
+	    cout << "\nlocally transposed data:" << endl;
+	    show(wholedata,X,Y,0,0,X,Y);
+	  }
+	}
+
+	bool success=true;
+	for(unsigned int i=0; i < X; ++i) {
+	  for(unsigned int j=0; j < Y; ++j) {
+	    unsigned int pos=i * Y + j; 
+	    if(wholedata[pos] != wholeoutput[pos])
+	      success=false;
+	  }
+	}
+	
+	if(success == true) {
+	  cout << "\nTest succeeded." << endl;
+	} else {
+	  cout << "\nERROR: TEST FAILED!" << endl;
+	  retval += 1;
+	}
+
+      }
+      
+    } else {
+      if(rank == 0)
+	cout << "Speed test.\n" << endl;
+      for(int k=0; k < N; ++k) {
+	init(data,X,y,Z,ystart);
+    
+	double begin=0.0, Tinit0=0.0, Tinit=0.0, Twait0=0.0, Twait1=0.0;
+	if(rank == 0) begin=totalseconds();
+
+	T.inphase0();
+	if(rank == 0) Tinit0=totalseconds();
+	T.insync0();
+	if(rank == 0) Twait0=totalseconds();
+	T.inphase1();
+	if(rank == 0) Tinit=totalseconds();
+	T.insync1();
+	if(rank == 0) Twait1=totalseconds();
+	T.inpost();
+
+	if(rank == 0) {
+	  Sin.add(totalseconds()-begin);
+	  Sininit.add(Tinit0-begin);
+	  Sinwait0.add(Twait0-Tinit0);
+	  Sinwait1.add(Twait1-Tinit);
+	}
+
+	// if(showoutput) {
+	//   if(rank == 0) cout << "\ntranspose:\n" << endl;
+	//   show(data,x,Y*Z,active);
+	// }
+
+	if(rank == 0) begin=totalseconds();
+	T.outphase0();
+	if(rank == 0) Tinit0=totalseconds();
+	T.outsync0();
+	if(rank == 0) Twait0=totalseconds();
+	T.outphase1();
+	if(rank == 0) Tinit=totalseconds();
+	T.outsync1();
+	if(rank == 0) Twait1=totalseconds();
+	if(outtranspose) T.NmTranspose();
+    
+	if(rank == 0) {
+	  Sout.add(totalseconds()-begin);
+	  Soutinit.add(Tinit0-begin);
+	  Soutwait0.add(Twait0-Tinit0);
+	  Soutwait1.add(Twait1-Tinit);
+	}
+      }
+  
+      // if(showoutput) {
+      // 	if(outtranspose) {
+      // 	  if(rank == 0) cout << "\nout:\n" << endl;
+      // 	  show(data,y,X*Z,active);
+      // 	} else {
+      // 	  if(rank == 0) cout << "\noriginal:\n" << endl;
+      // 	  show(data,X,y*Z,active);
+      // 	}
+      // }
+
+      if(rank == 0) {
+	Sininit.output("Tininit",X);
+	Sinwait0.output("Tinwait0",X);
+	Sinwait1.output("Tinwait1",X);
+	Sin.output("Tin",X);
+	cout << endl;
+	Soutinit.output("Toutinit",X);
+	Soutwait0.output("Toutwait0",X);
+	Soutwait1.output("Toutwait1",X);
+	Sout.output("Tout",X);
+      }
+
+    }
   
   }
+
+  return retval;
 }
 
 int main(int argc, char **argv)
 {
+  bool Nset = false;
+
 #ifdef __GNUC__ 
   optind=0;
 #endif  
@@ -301,40 +379,41 @@ int main(int argc, char **argv)
     if (c == -1) break;
                 
     switch (c) {
-      case 0:
-        break;
-      case 'N':
-        N=atoi(optarg);
-        break;
-      case 'L':
-        outtranspose=true;
-        break;
-      case 'm':
-        X=Y=atoi(optarg);
-        break;
-      case 'X':
-        X=atoi(optarg);
-        break;
-      case 'Y':
-        Y=atoi(optarg);
-        break;
-      case 'Z':
-        Z=atoi(optarg);
-        break;
-      case 'T':
-        fftw::maxthreads=atoi(optarg);
-        break;
-      case 'n':
-        N0=atoi(optarg);
-        break;
-      case 'h':
-      default:
-        usage();
+    case 0:
+      break;
+    case 'N':
+      N=atoi(optarg);
+      Nset = true;
+      break;
+    case 'L':
+      outtranspose=true;
+      break;
+    case 'm':
+      X=Y=atoi(optarg);
+      break;
+    case 'X':
+      X=atoi(optarg);
+      break;
+    case 'Y':
+      Y=atoi(optarg);
+      break;
+    case 'Z':
+      Z=atoi(optarg);
+      break;
+    case 'T':
+      fftw::maxthreads=atoi(optarg);
+      break;
+    case 'n':
+      N0=atoi(optarg);
+      break;
+    case 'h':
+    default:
+      usage();
     }
   }
 
   int provided;
-//  MPI_Init(&argc,&argv);
+  //  MPI_Init(&argc,&argv);
   MPI_Init_thread(&argc,&argv,MPI_THREAD_FUNNELED,&provided);
 
   int rank, size;
@@ -348,11 +427,17 @@ int main(int argc, char **argv)
   
   fftw_mpi_init();
   
+  int retval=0;
 #ifdef OLD
-  fftwTranspose(rank,size);
+  fftwTranspose(rank, size);
 #else
-  transpose(rank,size);
+  if(!Nset) {
+    N=N0/(X*Y);
+    if(N < 10) N=10;
+  }
+  transpose(rank,size, N);
 #endif  
   
   MPI_Finalize();
+  return 0;
 }
