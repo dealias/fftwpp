030f598500c2b86b266925af65bec0e3e3e41276
Malcolm Roberts <squidgyhead@gmail.com>
2015-10-11 13:01:18 +0200

    Improve output of tests/fft3r

--- tests/fft3r.cc
+++ tests/fft3r.cc
@@ -22,18 +22,19 @@ int main(int argc, char* argv[])
   unsigned int mx = 11;
   unsigned int my = 4;
   unsigned int mz = 3;
-    
+
+  unsigned int outlimit=3000;
+  
   int N = 1000;
   unsigned int stats = MEAN; // Type of statistics used in timing test.
 
   fftw::maxthreads = get_max_threads();
-  int r = -1;
  
 #ifdef __GNUC__	
   optind=0;
 #endif	
   for (;;) {
-    int c = getopt(argc,argv,"N:m:x:y:z:r:T:S:h");
+    int c = getopt(argc,argv,"N:m:x:y:z:T:S:h");
     if (c == -1) break;
     switch (c) {
     case 0:
@@ -44,14 +45,14 @@ int main(int argc, char* argv[])
     case 'm':
       mx = my = mz = atoi(optarg);
       break;
-    case 'r':
-      r = atoi(optarg);
-      break;
     case 'x':
       mx = atoi(optarg);
       break;
     case 'y':
       my = atoi(optarg);
+      break;
+    case 'z':
+      mz = atoi(optarg);
       break;
     case 'T':
       fftw::maxthreads = max(atoi(optarg), 1);
@@ -67,42 +68,46 @@ int main(int argc, char* argv[])
   }
 
   size_t align = sizeof(Complex);
+  unsigned int mzp = mz/2+1;
+  array3<double> f(mx,my,mz,align);
+  array3<Complex> g(mx,my,mzp,align);
 
-  unsigned int mzp = mz / 2 + 1;
-  
-  array3<Complex> g(mx, my, mzp, align);
-  
-  double *T = new double[N];
+  rcfft3d Forward0(mx,my,mz,f,g);
+  crfft3d Backward0(mx,my,mz,g,f);
 
-  // if(r == -1 || r == 0) {
-  //   rcfft2d Forward(m, f);
-  //   //crfft1d Backward(1, f);
-  //   for(int i = 0; i < N; ++i) {
-  //     finit(f, m);
-  //     seconds();
-  //     Forward.fft(f);
-  //     T[i] = seconds();
-  //     //Backward.fftNormalized(f);
-  //   }
+  finit(f,mx,my,mz);
+  cout << "\nIinput:"  << endl;
+  if(mx*my*mz < outlimit) 
+    cout << f;
+  else 
+    cout << f[0][0][0] << endl;
 
-  //   timings("fft1 in-place", m, T, N, stats);
-  // }
+  Forward0.fft(f,g);
 
-  if(r == -1 || r == 1) {
-    array3<double> f(mx,my,mz,align);
-    rcfft3d Forward0(mx,my,mz,f,g);
-    //crfft1d Backward0(1,g,f);
+  cout << "\nOutput:" << endl;
+  if(mx*my*mz < outlimit) 
+    cout << g;
+  else 
+    cout << g[0][0][0] << endl;
 
+  Backward0.fftNormalized(g,f);
+
+  cout << "\nBack to input:\n" << endl;
+  if(mx*my*mz < outlimit)
+    cout << f;
+  else 
+    cout << f[0][0][0] << endl;
+
+  if(N > 0) {
+    double *T = new double[N];
     for(int i = 0; i < N; ++i) {
-      finit(f, mx, my, mz);
+      finit(f,mx,my,mz);
       seconds();
-      Forward0.fft(f, g);
+      Forward0.fft(f,g);
       T[i] = seconds();
-      //Backward0.fftNormalized(g, f);
+      Backward0.fftNormalized(g, f);
     }
-
     timings("fft3 out-of-place", mx, T, N, stats);
   }
-
-  //cout << "\nback to input:\n" << f << endl;
+  
 }
