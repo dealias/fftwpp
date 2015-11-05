500246f5465ec84e3524ec457911e6cc175057bc
Malcolm Roberts <malcolmiwroberts@gmail.com>
2015-11-05 20:22:57 +0100

    Clean up examplecconv

--- examples/examplecconv.cc
+++ examples/examplecconv.cc
@@ -7,23 +7,14 @@
 
 using namespace std;
 using namespace utils;
-using namespace Array;
 using namespace fftwpp;
 
-// size of problem
-unsigned int m=8;
 
-unsigned int mx=4;
-unsigned int my=4;
-
-inline void init(Complex *f, Complex *g) 
+void init(Complex *f, Complex *g, unsigned int m) 
 {
-  unsigned int m1=m+1;
-  for(unsigned int i=0; i < m1; i += m1) {
-    Complex *fi=f+i;
-    Complex *gi=g+i;
-    for(unsigned int k=0; k < m; k++) fi[k]=Complex(k,k+1);
-    for(unsigned int k=0; k < m; k++) gi[k]=Complex(k,2*k+1);
+  for(unsigned int k=0; k < m; k++) {
+    f[k]=Complex(k,k+1);
+    g[k]=Complex(k,2*k+1);
   }
 }
 
@@ -31,6 +22,9 @@ int main(int argc, char* argv[])
 {
   fftw::maxthreads=get_max_threads();
 
+  // size of problem
+  unsigned int m=8;
+  
 #ifndef __SSE2__
   fftw::effort |= FFTW_NO_SIMD;
 #endif  
@@ -40,13 +34,17 @@ int main(int argc, char* argv[])
   Complex *g=ComplexAlign(m);
   
   cout << "1d non-centered complex convolution:" << endl;
-  init(f,g);
+  init(f,g,m);
   cout << "\ninput:\nf\tg" << endl;
   for(unsigned int i=0; i < m; i++) 
     cout << f[i] << "\t" << g[i] << endl;
   
   ImplicitConvolution C(m);
+
   C.convolve(f,g);
+
+  cout << "\noutput:" << endl;
+  for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
   
   cout << "\noutput:" << endl;
   for(unsigned int i=0; i < m; i++) cout << f[i] << endl;
