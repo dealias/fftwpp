574131101c75cf94f2ff2fb6b7711b274bfdf602
Malcolm Roberts <malcolmiwroberts@gmail.com>
2015-11-12 22:50:06 +0100

    Clean up tests/timing.py

--- tests/timing.py
+++ tests/timing.py
@@ -75,35 +75,42 @@ def default_outdir(p):
     outdir=""
 
     if p == "cconv":
-        outdir="timings1c"
+        outdir = "timings1c"
+        
     if p == "cconv2":
-        outdir="timings2c"
+        outdir = "timings2c"
+        
     if p == "cconv3":
-        outdir="timings3c"
+        outdir = "timings3c"
 
     if p == "conv":
-        outdir="timings1r"
+        outdir = "timings1r"
+        
     if p == "conv2":
-        outdir="timings2r"
+        outdir = "timings2r"
+        
     if p == "conv3":
-        outdir="timings3r"
+        outdir = "timings3r"
 
     if p == "tconv":
-        outdir="timings1t"
+        outdir = "timings1t"
+        
     if p == "tconv2":
         outdir="timings2t"
 
     if p == "fft1":
-        outdir="timingsf1"
+        outdir = "timingsf1"
+        
     if p == "mfft1":
-        outdir="timingsmf1"
+        outdir = "timingsmf1"
+        
     if p == "fft2":
-        outdir="timingsf2"
+        outdir = "timingsf2"
+        
     if p == "transpose":
         outdir="transpose2"
 
     return outdir
-
 
 def main(argv):
     usage = '''Usage:
@@ -124,25 +131,21 @@ def main(argv):
     -N<int> Number of tests to perform
     '''
 
-    dryrun=False
-    bset=0
-    dorun=True
-    Tset=False
-    T=1
-    p=""
-    cargs=""
-    B=[]
-    A=[]
-    a=6
-    b=0
-    out=""
-    runtype="implicit"
-    RAM=0
-    outdir=""
-    outfile=""
-    rname=""
-    N=0
-
+    dryrun = False
+    bset = 0
+    dorun = True
+    T = 0 # number of threads
+    p = "" # program name
+    B = [] # precommands
+    A = [] # postcommands
+    a = 6  # minimum log of problem size
+    b = 0  # maximum log of problem size
+    runtype = "implicit"  # type of run
+    RAM = 0  # ram limit in GB
+    outdir = ""  # output directory
+    outfile = "" # output filename
+    rname = ""   # output grep string
+    N = 0        # number of tests
 
     try:
         opts, args = getopt.getopt(argv,"hdp:T:a:b:A:B:r:R:o:D:g:N:")
@@ -152,33 +155,31 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-p"):
-            p=arg
+            p = arg
         if opt in ("-T"):
-            Tset=True
-            cargs+=" -T"+str(arg)
-            T=arg
+            T = arg
         elif opt in ("-a"):
-            a=int(arg)
+            a = int(arg)
         elif opt in ("-N"):
-            N=int(arg)
+            N = int(arg)
         elif opt in ("-b"):
-            b=int(arg)
+            b = int(arg)
         elif opt in ("-A"):
-            A+=[str(arg)]
+            A += [str(arg)]
         elif opt in ("-B"):
-            B+=[str(arg)]
+            B += [str(arg)]
         elif opt in ("-r"):
-            runtype=str(arg)
+            runtype = str(arg)
         elif opt in ("-R"):
-            RAM=float(arg)*2**30
+            RAM = float(arg)*2**30
         elif opt in ("-d"):
-            dryrun=True
+            dryrun = True
         elif opt in ("-o"):
-            out=str(arg)
+            outfile = str(arg)
         elif opt in ("-D"):
-            outdir=str(arg)
+            outdir = str(arg)
         elif opt in ("-g"):
-            rname=str(arg)
+            rname = str(arg)
         elif opt in ("-h"):
             print usage
             sys.exit(0)
@@ -193,52 +194,54 @@ def main(argv):
 
     # if both the max problem size and the ram are unset, go up to 2^8
     if (b == 0 and RAM == 0):
-        b=8
-    # if RAM is set and the max problem size is not set, then RAM
-    # determines the problem size on its own.
-    if (b == 0 and RAM != 0):
-        b=sys.maxint
+        b = 8
 
-    hermitian=False
-    ternary=False
+    hermitian = False
+    ternary = False
 
     if p == "cconv":
         if(runtype == "pruned"):
-            print p+" has no pruned option"
-            dorun=False
+            print p + " has no pruned option"
+            dorun = False
 
     if p == "conv":
-        hermitian=True
+        hermitian = True
         if(runtype == "pruned"):
-            print p+" has no pruned option"
-            dorun=False
+            print p + " has no pruned option"
+            dorun = False
+            
     if p == "conv2":
-        hermitian=True
+        hermitian = True
+        
     if p == "conv3":
-        hermitian=True
+        hermitian = True
         if(runtype != "implicit"):
-            print p+" has no "+r+" option"
-            dorun=False
+            print p + " has no " + r + " option"
+            dorun = False
 
     if p == "tconv":
-        ternary=True
+        ternary = True
         if(runtype == "pruned"):
-            print p+" has no pruned option"
-            dorun=False
+            print p + " has no pruned option"
+            dorun = False
+            
     if p == "tconv2":
-        ternary=True
+        ternary = True
 
     if p == "fft1":
-        runtype="fft"
+        runtype = "fft"
+        
     if p == "mfft1":
-        runtype="fft"
+        runtype = "fft"
+        
     if p == "fft2":
-        runtype="fft"
+        runtype = "fft"
+        
     if p == "transpose":
-        runtype="transpose"
+        runtype = "transpose"
 
     if outdir == "":
-        outdir=default_outdir(p)
+        outdir = default_outdir(p)
 
     if outdir == "":
         print "empty outdir: please select a program or specify an outdir (-D)"
@@ -250,19 +253,17 @@ def main(argv):
         b = max_m(p, RAM, runtype)
         print "max value of b with ram provided:", b
         
-    if out == "":
+    if outfile == "":
         if runtype == "implicit":
-            outfile="implicit"
+            outfile = "implicit"
         if runtype == "explicit":
-            outfile="explicit"
+            outfile = "explicit"
         if runtype == "pruned":
-            outfile="pruned"
+            outfile = "pruned"
         if runtype == "fft":
-            outfile="fft"
+            outfile = "fft"
         if runtype == "transpose":
-            outfile="tranpose"
-    else:
-        outfile=out
+            outfile = "tranpose"
 
     if dorun:
         if RAM != 0:
@@ -270,92 +271,98 @@ def main(argv):
 
         if rname == "":
             if runtype == "implicit":
-                rname="Implicit"
+                rname = "Implicit"
             if runtype == "explicit":
-                rname="Explicit"
+                rname = "Explicit"
             if runtype == "pruned":
-                rname="rune"
+                rname = "rune"
             if runtype == "fft":
-                rname="fft"
+                rname = "fft"
             if runtype == "transpose":
-                rname="transpose"
+                rname = "transpose"
 
-        print "Search string for timing: "+rname
+        print "Search string for timing: " + rname
 
-        print "output in "+outdir+"/"+outfile
+        print "output in " + outdir + "/" + outfile
 
         if not dryrun:
-            os.system("mkdir -p "+outdir)
-            os.system("rm -f "+outdir+"/"+outfile)
+            os.system("mkdir -p " + outdir)
+            os.system("rm -f " + outdir + "/" + outfile)
 
-        cmd=[]
-        i=0
+        cmd = []
+        i = 0
         while i < len(B):
             cmd.append(B[i]);
             i += 1
 
-        cmd+=["./"+str(p)]
-
-        if(N > 0):
-            cmd.append("-N" + str(N))
-
+        cmd += ["./" + str(p)]
+        
         if(runtype == "explicit"):
             cmd.append("-e")
+            
         if(runtype == "pruned"):
             cmd.append("-p")
+            
         if(runtype == "implicit"):
             cmd.append("-i")
-        if(Tset):
-            cmd.append("-T"+T)
+            if (p == "conv2" or p == "conv3"):
+                cmd += ["-X1"]
+                cmd += ["-Y1"]
+                if(p == "conv3"):
+                    cmd += ["-Z1"]
+        
+        if(N > 0):
+            cmd.append("-N" + str(N))
+        if(T > 0):
+            cmd.append("-T" + T)
 
         # Add the extra arguments to the program being timed.
-        i=0
+        i = 0
         while i < len(A):
             cmd.append(A[i]);
             i += 1
             
         print cmd
 
-        for i in range(a,b+1):
+        for i in range(a, b + 1):
             if not hermitian or runtype == "implicit": 
-                m=str(int(pow(2,i)))
+                m = str(int(pow(2, i)))
             else:
                 if not ternary:
-                    m=str(int(floor((pow(2,i+1)+2)/3)))
+                    m = str(int(floor((pow(2, i + 1) + 2) / 3)))
                 else:
-                    m=str(int(floor((pow(2,i+2)+3)/4)))
+                    m = str(int(floor((pow(2, i + 2) + 3) / 4)))
                     
-            print str(i)+" m="+str(m)
+            print str(i) + " m=" + str(m)
             
-
-            mcmd=cmd+["-m"+str(m)]
+            mcmd = cmd + ["-m" + str(m)]
 
             if dryrun:
                 print mcmd
             else:
-                p=Popen(mcmd,stdout=PIPE,stderr=PIPE)
+                p = Popen(mcmd, stdout = PIPE, stderr = PIPE)
                 p.wait() # sets the return code
-                prc=p.returncode
+                prc = p.returncode
                 out, err = p.communicate() # capture output
                 if (prc == 0): # did the process succeed?
                     #print out
-                    outlines=out.split('\n')
-                    itline=0
-                    dataline=""
+                    outlines = out.split('\n')
+                    itline = 0
+                    dataline = ""
                     while itline < len(outlines):
-                        line=outlines[itline]
+                        line = outlines[itline]
                         #print line
-                        re.search(rname,line)
+                        re.search(rname, line)
                         if re.search(rname, line) is not None:
-                            print "\t"+str(outlines[itline])
-                            print "\t"+str(outlines[itline+1])
-                            dataline=outlines[itline+1]
-                            itline=len(outlines)
+                            print "\t" + str(outlines[itline])
+                            print "\t" + str(outlines[itline + 1])
+                            dataline = outlines[itline + 1]
+                            itline = len(outlines)
                         itline += 1
                     if not dataline == "":
                         # append to output file
-                        with open(outdir+"/"+outfile, "a") as myfile:
-                            myfile.write(dataline+"\n")
+                        with open(outdir + "/" + outfile, "a") as myfile:
+                            myfile.write(dataline + "\n")
                     else:
                         print "ERROR: no timing data found"
                 else:
