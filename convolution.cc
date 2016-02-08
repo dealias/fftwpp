dealias/fftwpp

commit 884f376f030ce9f6fe53fc1e5dd801e7e25297d7

Fix autoconvolution for cconv

convolution.cc
@@ -54,19 +54,21 @@ void ImplicitConvolution::convolve(Complex **F,
     P[i]=F[i]+offset;
   
   // Backwards FFT (even indices):
-  for(unsigned int i=0; i < A; ++i)
+  for(unsigned int i=0; i < A; ++i) {
+    //std::cout << P[i] << " " << U[i] << std::endl;
     BackwardsO->fft(P[i],U[i]);
+  }
   (*pmult)(U,m,threads); // multiply even indices
 
   switch(A) {
-    case 1: premult<premult1>(P); break;
-    case 2: premult<premult2>(P); break;
-    case 3: premult<premult3>(P); break;
-    case 4: premult<premult4>(P); break;
-    default: premult<general>(P);
+  case 1: premult<premult1>(P); break;
+  case 2: premult<premult2>(P); break;
+  case 3: premult<premult3>(P); break;
+  case 4: premult<premult4>(P); break;
+  default: premult<general>(P);
   }
 
-  if(A > B) { // out-of-place FFTs: U[A-1] is free if A > B.
+  if(out_of_place) { // out-of-place FFTs: U[A-1] is free if A > B.
     Complex *W[A];
     W[A-1]=U[A-1];
     for(unsigned int i=1; i < A; ++i) 
@@ -95,11 +97,11 @@ void ImplicitConvolution::convolve(Complex **F,
 
     // Return to original space:
     for(unsigned int i=0; i < B; ++i) {
-      Complex *f=P[i];
-      Complex *u=U[i];
-      Forwards->fft(f);
-      Forwards->fft(u);
-      postmultadd(f,u);
+      Complex *fi=P[i];
+      Complex *ui=U[i];
+      Forwards->fft(fi);
+      Forwards->fft(ui);
+      postmultadd(fi,ui);
     }
   }
 }

convolution.h
@@ -124,6 +124,7 @@ private:
   fft1d *Backwards,*Forwards;
   bool pointers;
   bool allocated;
+  bool out_of_place;
 public:
   void initpointers(Complex **&U, Complex *u) {
     unsigned int C=max(A,B);
@@ -138,14 +139,15 @@ public:
   }
 
   void init() {
+    out_of_place = A < B;
+
     Complex* U0=U[0];
     Complex* U1=A == 1 ? ComplexAlign(m) : U[1];
     
     BackwardsO=new fft1d(m,1,U0,U1);
-
-    if(A < B) {
-      Backwards=new fft1d(m,1,U0);
-      Forwards=new fft1d(m,-1,U0);
+    Backwards=new fft1d(m,1,U0);
+    Forwards=new fft1d(m,-1,U0);
+    if(out_of_place) {
       threads=std::min(threads,
                        std::max(Backwards->Threads(),Forwards->Threads()));
     } else {

fftw++.h
@@ -238,6 +238,8 @@ public:
   
   virtual unsigned int Threads() {return threads;}
   
+  bool get_inplace() {return inplace;}
+
   static const char *oddshift;
   
   // Inplace shift of Fourier origin to (nx/2,0) for even nx.

tests/cconv.cc
@@ -21,7 +21,21 @@ void xcorr_mult(Complex **F, unsigned int m, unsigned int threads)
   Complex* F0=F[0];
   Complex* F1=F[1];
   for(unsigned int j=0; j < m; ++j)
-    F0[j] *= F1[j];
+    F0[j] *= conj(F1[j]);
+}
+
+void autocorr_mult(Complex **F, unsigned int m, unsigned int threads)
+{
+  Complex* F0=F[0];
+  for(unsigned int j=0; j < m; ++j)
+    F0[j] *= conj(F0[j]);
+}
+
+void autoconv_mult(Complex **F, unsigned int m, unsigned int threads)
+{
+  Complex* F0=F[0];
+  for(unsigned int j=0; j < m; ++j)
+    F0[j] *= F0[j];
 }
 
 inline void init(Complex **F, unsigned int m, unsigned int A) 
@@ -48,7 +62,7 @@ inline void init(Complex **F, unsigned int m, unsigned int A)
   } else {
     for(unsigned int a=0; a < A; ++a) {
       for(unsigned int k=0; k < m; ++k) {
-	F[a][k]=a*Complex(k,k+1);
+	F[a][k]=(a+1)*Complex(k,k+1);
       }
     }
   }
@@ -159,12 +173,19 @@ int main(int argc, char* argv[])
   double *T=new double[N];
   
   if(Implicit) {
-    ImplicitConvolution C(m,A,B);
+    
+    Complex **U=new Complex *[A];
+    for(unsigned int a=0; a < A; ++a)
+      U[a]=ComplexAlign(m);
+    // FIXME: A=1, B=1 doesn't automatically create correct U array.
+    
+    ImplicitConvolution C(m,U,A,B);
     fftw::SaveWisdom();
     cout << "threads=" << C.Threads() << endl << endl;;
 
     multiplier *mult;
     switch(A) {
+    case 1: mult=autoconv_mult; break;
     case 2: mult=multbinary; break;
     case 4: mult=multbinary2; break;
     case 6: mult=multbinary3; break;

commit 21dafc58ab0cdb923908f4279b465378f190c9fe

Fix out-of-place bug introduced in 884f376f030ce9f6fe53fc1e5dd801e7e25297d7. Also move nonglobal even flag back to where it is needed for (compactness and cache friendliness).

convolution.cc
@@ -73,7 +73,7 @@ void ImplicitConvolution::convolve(Complex **F, multiplier *pmult,
     default: premult<general>(P);
   }
 
-  if(out_of_place) { // out-of-place FFTs: U[A-1] is free if A > B.
+  if(outofplace) { // out-of-place FFTs: U[A-1] is free if A > B.
     Complex *W[A];
     W[A-1]=U[A-1];
     for(unsigned int i=1; i < A; ++i) 
@@ -228,7 +228,8 @@ void ImplicitHConvolution::premult(Complex ** F,
 
   Vec Mhalf=LOAD(-0.5);
   Vec HSqrt3=LOAD(hsqrt3);
-    
+  
+  bool even=m == 2*c;
   if(even)
     S=new Complex[A];
   unsigned int m1=m-1;
@@ -358,6 +359,7 @@ void ImplicitHConvolution::postmultadd0(Complex **c2, Complex **c0,
 {
   double ninv=1.0/(3.0*m);
   Vec Ninv=LOAD(ninv);
+  bool even=m == 2*c;
 
   Vec Mhalf=LOAD(-0.5);
   Vec HSqrt3=LOAD(hsqrt3);
@@ -490,7 +492,7 @@ void ImplicitHConvolution::convolve(Complex **F, realmultiplier *pmult,
     c2[a]=U[a];
   }
   
-  if(out_of_place) { 
+  if(outofplace) { 
     for(unsigned int a=0; a < A-1; ++a) {
       d0[a]=(double *) c0[a+1];
       d1[a]=(double *) c1[a+1];
@@ -505,6 +507,7 @@ void ImplicitHConvolution::convolve(Complex **F, realmultiplier *pmult,
 
   premult(F,offset,c1c);
 
+  bool even=m == 2*c;
   // Complex-to-real FFTs and pmults:
   Complex *S=new Complex[B];
   {
@@ -520,7 +523,7 @@ void ImplicitHConvolution::convolve(Complex **F, realmultiplier *pmult,
       T[a]=c0a[0].re; // r=0, k=0
       if(!compact)
 	c0a[0].re += 2.0*c0a[m].re; // Nyquist
-      (out_of_place ? cro : cr)->fft(c0a,d0[a]);
+      (outofplace ? cro : cr)->fft(c0a,d0[a]);
     }
     (*pmult)(d0,m,indexsize,index,0,threads);
     for(unsigned int b=0; b < B; ++b)
@@ -535,13 +538,13 @@ void ImplicitHConvolution::convolve(Complex **F, realmultiplier *pmult,
         c1c[a]=c1a[1];  // r=0, k=c
         c1a[1]=tmp;     // r=1, k=1
       }
-      (out_of_place ? cro : cr)->fft(c1[a],d1[a]);
+      (outofplace ? cro : cr)->fft(c1[a],d1[a]);
     }
     (*pmult)(d1,m,indexsize,index,1,threads);
   }
 
   // Real-to-complex FFTs and postmultadd:
-  if(out_of_place) {
+  if(outofplace) {
     // Put d1 into the second half of c0:
     Complex **c2B=c2+B;
 

convolution.h
@@ -119,7 +119,7 @@ private:
   fft1d *Backwards,*Forwards;
   bool pointers;
   bool allocated;
-  bool out_of_place;
+  bool outofplace;
   unsigned int indexsize;
 public:
   unsigned int *index;
@@ -143,7 +143,7 @@ public:
   
   void init() {
     indexsize=0;
-    out_of_place = A < B;
+    outofplace=A > B;
 
     Complex* U0=U[0];
     Complex* U1=A == 1 ? utils::ComplexAlign(m) : U[1];
@@ -151,14 +151,13 @@ public:
     BackwardsO=new fft1d(m,1,U0,U1);
     Backwards=new fft1d(m,1,U0);
     Forwards=new fft1d(m,-1,U0);
-    if(out_of_place) {
-      threads=std::min(threads,
-                       std::max(Backwards->Threads(),Forwards->Threads()));
-    } else {
+    if(outofplace) {
       ForwardsO=new fft1d(m,-1,U0,U1);
       threads=std::min(threads,
                        std::max(BackwardsO->Threads(),ForwardsO->Threads()));
-    }
+    } else
+      threads=std::min(threads,
+                       std::max(Backwards->Threads(),Forwards->Threads()));
     
     if(A == 1) utils::deleteAlign(U1);
 
@@ -208,13 +207,12 @@ public:
     if(pointers) deletepointers(U);
     if(allocated) utils::deleteAlign(u);
     
-    delete ForwardsO;
-    delete BackwardsO;    
-    if(out_of_place) {
-      delete Forwards;
-      delete Backwards;
-    }
+    if(outofplace)
+      delete ForwardsO;
     
+    delete Forwards;
+    delete Backwards;
+    delete BackwardsO;    
   }
 
   // F is an array of A pointers to distinct data blocks each of size m,
@@ -270,8 +268,7 @@ protected:
   bool pointers;
   bool allocated;
   unsigned int indexsize;
-  bool out_of_place;
-  bool even;
+  bool outofplace;
 public:
   unsigned int *index;
 
@@ -309,8 +306,7 @@ public:
     threads=std::min(threads,std::max(rco->Threads(),cro->Threads()));
     s=BuildZeta(3*m,c+2,ZetaH,ZetaL,threads);
 
-    out_of_place=A >= 2*B;
-    even=m == 2*c;
+    outofplace=A >= 2*B;
   }
   
   // m is the number of independent data values