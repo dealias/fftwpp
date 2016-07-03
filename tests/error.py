8b0c110e6626f951740f97f2460266bb071afa29
Malcolm Roberts <malcolmiwroberts@gmail.com>
2016-07-03 22:32:45 +0200

    Add more test cases to tests/error.py

--- tests/error.py
+++ tests/error.py
@@ -13,57 +13,90 @@ retval=0
 
 print "Comparison of routine versus test case:"
 print
-print "program\ttype    \tlength\t\terror:"
+
+# problem sizes used in test:
+mlist = [1, 2, 8, 9, 1024, random.randint(10, 2048)] 
+
+Alist = [2, 4]
+Blist = [1, 2]
+
+# Implicit convolution tests.
+plist = ["cconv", "conv"]
+convtypes = ["implicit", "explicit"]
+
+# Create the list of commands to be run:
+cmds = []
+for prog in plist:
+    if (os.path.isfile(prog)):
+        # Explicit version: A=2, B=1 only:
+        for mval in mlist:
+            cmd = []
+            cmd.append("./" + prog)
+            cmd.append("-N1")
+            cmd.append("-e")
+            cmd.append("-t")
+            cmd.append("-m" + str(mval))
+            cmds.append(cmd)
+
+        # Implicit version:
+        for mval in mlist:
+            for A in Alist:
+                for B in Blist:
+                    if B <= A:
+                        cmd = []
+                        cmd.append("./" + prog)
+                        cmd.append("-N1")
+                        cmd.append("-i")
+                        cmd.append("-t")
+                        cmd.append("-m" + str(mval))
+                        cmd.append("-A" + str(A))
+                        cmd.append("-B" + str(B))
+                        cmds.append(cmd)
+    else:
+        print(prog + " does not exist; please compile.")
+        retval += 1
+
+# Find the length of the text output for pretty formatting
+maxcmdlen = 0
+for cmd in cmds:
+    thislen = len(" ".join(cmd))
+    if thislen > maxcmdlen:
+        maxcmdlen = thislen
+
+# Run the commands and store the failed cases
+print "command".ljust(maxcmdlen) + "\terror"
+print
+failures = []
+for cmd in cmds:
+    p = Popen(cmd, stdout = PIPE, stderr = PIPE)
+    p.wait()
+    out, err = p.communicate() # capture output
+    if (p.returncode == 0): # did the process succeed?
+        # Find the text after "error=":
+        m = re.search("(?<=error=)(.*)", out) 
+        errval = float(m.group(0))
+        print " ".join(cmd).ljust(maxcmdlen) + "\t" +  str(errval),
+        
+        if(errval > 1e-10):
+            print "\tERROR TOO LARGE"
+            failures.append([cmd, "error: " + str(errval)])
+            retval += 1
+        else:
+            print
+    else:
+        print " ".join(cmd) + "\tFAILED"
+        failures.append([cmd, "return code: " + str(p.returncode)])
+        retval += 1
+
 print
 
-mlist=[1,2,8,9,1024,random.randint(10,2048)] # problem sizes used in test.
-ilist=["cconv", "conv"]
-for prog in ilist:
-    if (os.path.isfile(prog)):
-        for mval in mlist:
-            p=Popen(["./"+prog,"-N1","-i","-t","-m"+str(mval)],stdout=PIPE,stderr=PIPE)
-            p.wait() # sets the return code
-            out, err = p.communicate() # capture output
-            if (p.returncode == 0): # did the process succeed?
-                m=re.search("(?<=error=)(.*)",out) # find the text after "error="
-                print prog +"\t"+"implicit\t"+str(mval)+"\t"+m.group(0)
-                if(float(m.group(0)) > 1e-10):
-                    print "ERROR TOO LARGE"
-                    retval+=1
-            else:
-                print prog+" FAILED"
-                retval+=1
-    else:
-        print(prog+" does not exist; please compile.")
-        retval+=1
-
-
-elist=["cconv","conv"] 
-for prog in elist:
-    if (os.path.isfile(prog)):
-        for mval in mlist:
-            p=Popen(["./"+prog,"-N1","-e","-t"],stdout=PIPE,stderr=PIPE)
-            p.wait() # sets the return code
-            out, err = p.communicate() # capture output
-            if (p.returncode == 0): # did the process succeed?
-                m=re.search("(?<=error=)(.*)",out) # find the text after "error="
-                print prog +"\t"+"explicit\t"+str(mval)+"\t"+m.group(0)
-                if(float(m.group(0)) > 1e-10):
-                    print "ERROR TOO LARGE"
-                    retval+=1
-            else:
-                print prog+" FAILED"
-                retval+=1
-    else:
-        print(prog+" does not exist; please compile.")
-        retval+=1
-
-
-print
-
-if(retval == 0):
-    print "OK\tall tests passed"
+if retval == 0:
+    print "OK: all tests passed."
 else:
-    print "ERROR\tAT LEAST ONE TEST FAILED"
+    print "Failed cases:"
+    for fail in failures:
+        print " ".join(fail[0]).ljust(maxcmdlen) + "\t" + fail[1] 
+    print
+    print "ERROR: " + str(len(failures)) + " TEST(S) FAILED"
 
 sys.exit(retval)
