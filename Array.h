c2fa0d4d3bc1c7be027a97f076c45839e40605e3
Patrik Schönfeldt <patrik.schoenfeldt@kit.edu>
2018-04-18 22:24:06 +0200

    Added iterators to Array.h

--- Array.h
+++ Array.h
@@ -1,5 +1,6 @@
 /* Array.h:  A high-performance multi-dimensional C++ array class
    Copyright (C) 1997-2016 John C. Bowman, University of Alberta
+   Copyright (C) 2018 Patrik Schoenfeldt, Karlsruhe Institute of Technology
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -368,8 +369,366 @@ public:
     }
     return norm;
   }
-#endif  
-};
+#endif
+private: // abstract iterator prototypes
+  /**
+   * @brief The NDarray1::abstract_iterator class serves
+   *  as base class for NDarray1::const_iterator, NDarray1::iterator,
+   *  and NDarray1::const_iterator
+   */
+  class abstract_iterator
+  {
+  public:
+    /**
+     * @brief value_type std::iterator_traits::value_type
+     */
+    typedef T value_type;
+
+    /**
+     * @brief reference std::iterator_traits::reference
+     */
+    typedef T& reference;
+
+    /**
+     * @brief pointer std::iterator_traits::pointer
+     */
+    typedef T* pointer;
+
+    /**
+     * @brief iterator_category std::iterator_traits::iterator_category
+     */
+    typedef std::random_access_iterator_tag iterator_category;
+
+    /**
+     * @brief difference_type std::iterator_traits::difference_type
+     */
+    typedef std::ptrdiff_t difference_type;
+
+    friend void swap(abstract_iterator& lhs, abstract_iterator& rhs)
+    { std::swap(lhs._ptr,rhs._ptr); }
+
+    /**
+     * @brief operator =
+     * @param other
+     * @return
+     */
+    abstract_iterator& operator=(abstract_iterator& other)
+    { swap(*this, other); return *this; }
+
+    /* constructors are marked protected,
+     * so that no abstract_iterator is constructed */
+  protected:
+    /**
+     * @brief abstract_iterator default and initializing constructors
+     * @param ptr
+     */
+    abstract_iterator(T* ptr=nullptr) : _ptr(ptr) {}
+
+  public: // arithmetic operators
+    virtual inline abstract_iterator
+    operator+=(const std::ptrdiff_t& n)
+    { _ptr += n; return *this; }
+
+    virtual inline const abstract_iterator
+    operator+(const std::ptrdiff_t& n) const
+    { return abstract_iterator(*this) += n; }
+
+    virtual inline abstract_iterator operator++()
+    { _ptr++; return *this; }
+
+    virtual inline abstract_iterator operator++(int)
+    { abstract_iterator i = *this; _ptr++; return i; }
+
+    virtual inline abstract_iterator operator-=(const std::ptrdiff_t& n)
+    { _ptr -= n; return *this; }
+
+    virtual inline abstract_iterator operator--()
+    { _ptr--; return *this; }
+
+    virtual inline abstract_iterator operator--(int)
+    { abstract_iterator i = *this; _ptr--; return i; }
+
+    virtual inline const abstract_iterator
+    operator-(const std::ptrdiff_t& n) const
+    { return abstract_iterator(*this) -= n; }
+
+    friend inline const abstract_iterator&
+    operator+( const std::ptrdiff_t& lhs, const abstract_iterator& rhs)
+    { return rhs+lhs; }
+
+    friend inline const abstract_iterator
+    operator-( const std::ptrdiff_t& lhs, const abstract_iterator& rhs)
+    { return rhs-lhs; }
+
+  public: // comparission operators
+    inline bool operator==(const abstract_iterator& other)
+    { return _ptr == other._ptr; }
+
+    inline bool operator!=(const abstract_iterator& other)
+    { return !(*this == other); }
+
+    virtual inline bool operator>(const abstract_iterator& other)
+    { return _ptr > other._ptr; }
+
+    virtual inline bool operator<(const abstract_iterator& other)
+    { return _ptr < other._ptr; }
+
+    virtual inline bool operator>=(const abstract_iterator& other)
+    { return !operator<(other); }
+
+    virtual inline bool operator<=(const abstract_iterator& other)
+    { return !operator>(other); }
+
+  public:
+    /* access operators (const and non-const iterators use them) */
+    inline const T& operator[](std::ptrdiff_t& n) const
+    { return *(abstract_iterator::_ptr + n); }
+
+    inline const T& operator*() const
+    { return *abstract_iterator::_ptr; }
+
+    inline const T* operator->() const
+    { return abstract_iterator::_ptr; }
+
+  protected:
+    T* _ptr;
+  };
+
+  class abstract_reverse_iterator: public abstract_iterator
+  {
+  protected:
+      // default and initializing constructors
+      abstract_reverse_iterator(T* ptr=nullptr)
+          : abstract_iterator((ptr==nullptr)?nullptr:ptr-1) {}
+
+  public:
+    virtual inline abstract_iterator
+    operator+=(const std::ptrdiff_t& n)
+        { return abstract_iterator::operator -=(n); }
+
+        virtual inline const abstract_iterator
+        operator+(const std::ptrdiff_t& n) const
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator-(n); }
+
+        virtual inline abstract_iterator operator++()
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator--(); }
+
+        virtual inline abstract_iterator operator++(int)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator--(0); }
+
+        virtual inline abstract_iterator
+        operator-=(const std::ptrdiff_t& n)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator +=(n); }
+
+        virtual inline abstract_iterator operator--()
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator ++(); }
+
+        virtual inline abstract_iterator operator--(int)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator ++(0); }
+
+        virtual inline const abstract_iterator
+        operator-(const std::ptrdiff_t& n) const
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator+(n); }
+
+    public: // comparission operators
+        inline bool operator>(const abstract_iterator& other)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator<(other); }
+
+        inline bool operator<(const abstract_iterator& other)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator>(other); }
+
+        inline bool operator>=(const abstract_iterator& other)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator<=(other); }
+
+        inline bool operator<=(const abstract_iterator& other)
+        #if __cplusplus == 201103L
+        final
+        #endif
+        { return abstract_iterator::operator>=(other); }
+    };
+
+
+
+public: // actual iterators
+    /**
+     * @brief The NDarray1::iterator class
+     */
+    class iterator : public abstract_iterator
+    {
+    public:
+        // default and initializing constructors
+        iterator(T* ptr=nullptr)
+        : abstract_iterator(ptr) {}
+
+    public: // access operators
+        inline T& operator[](std::ptrdiff_t n)
+        { return const_cast<T&>(abstract_iterator::operator[](n)); }
+
+        inline T& operator*()
+        { return const_cast<T&>(abstract_iterator::operator*()); }
+
+        inline T* operator->()
+        { return const_cast<T&>(abstract_iterator::operator->()); }
+    };
+
+    /**
+     * @brief The NDarray1::reverse_iterator class
+     */
+    class reverse_iterator : public abstract_reverse_iterator
+    {
+    public:
+        // default and initializing constructors
+        reverse_iterator(T* ptr=nullptr)
+        : abstract_reverse_iterator(ptr) {}
+
+    public: // access operators
+        inline T& operator[](std::ptrdiff_t n)
+        { return const_cast<T&>(abstract_iterator::operator[](-n)); }
+
+        inline T& operator*()
+        { return const_cast<T&>(abstract_iterator::operator*()); }
+
+        inline T* operator->()
+        { return const_cast<T&>(abstract_iterator::operator->()); }
+    };
+
+    /**
+     * @brief The NDarray1::const_iterator class
+     */
+    class const_iterator : public abstract_iterator
+    {
+    public:
+        // default and initializing constructors
+        const_iterator(T* ptr=nullptr)
+        : abstract_iterator(ptr) {}
+
+    public: // access operators
+        inline const T& operator[](std::ptrdiff_t n)
+        { return (abstract_iterator::operator[](n)); }
+
+        inline const T& operator*()
+        { return (abstract_iterator::operator*()); }
+
+        inline const T* operator->()
+        { return (abstract_iterator::operator->()); }
+    };
+
+    /**
+     * @brief The NDarray1::const_reverse_iterator class
+     */
+    class const_reverse_iterator: public abstract_reverse_iterator
+    {
+    public:
+        // default and initializing constructors
+        const_reverse_iterator(T* ptr=nullptr)
+        : abstract_reverse_iterator(ptr) {}
+
+    public: // access operators
+        inline const T& operator[](std::ptrdiff_t n)
+        { return (abstract_iterator::operator[](-n)); }
+
+        inline const T& operator*()
+        { return (abstract_iterator::operator*()); }
+
+        inline const T* operator->()
+        { return (abstract_iterator::operator->()); }
+    };
+
+public: // iterator geters
+  iterator begin()
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return iterator(array1<T>::v); }
+
+  iterator end()
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return iterator(array1<T>::v + array1<T>::size); }
+
+  reverse_iterator rbegin()
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return reverse_iterator(array1<T>::v + array1<T>::size); }
+
+  reverse_iterator rend()
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return reverse_iterator(array1<T>::v); }
+
+  const_iterator begin() const
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return const_iterator(array1<T>::v); }
+
+  const_iterator end() const
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return const_iterator(array1<T>::v + array1<T>::size); }
+
+  #if __cplusplus == 201103L
+  const_iterator cbegin() const noexcept
+  { return const_iterator(array1<T>::v); }
+  
+  const_iterator cend() const noexcept
+  { return const_iterator(array1<T>::v + array1<T>::size); }
+  #endif
+
+  const_reverse_iterator rbegin() const
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return const_reverse_iterator(array1<T>::v + array1<T>::size); }
+
+  const_reverse_iterator rend() const
+  #if __cplusplus == 201103L
+  noexcept
+  #endif
+  { return const_reverse_iterator(array1<T>::v); }
+
+  #if __cplusplus == 201103L
+  const_reverse_iterator crbegin() const noexcept
+  { return const_reverse_iterator(array1<T>::v + array1<T>::size); }
+
+  const_reverse_iterator crend() const noexcept
+  { return const_reverse_iterator(array1<T>::v); }
+  #endif
+}; // array1
 
 template<class T>
 void swaparray(T& A, T& B)
