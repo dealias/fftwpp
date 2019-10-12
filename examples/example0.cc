ec6b82cc1122ba029a7a7142cf836014e992e68c
John Bowman <bowman@ualberta.ca>
2019-10-11 21:29:49 -0600

    Comment out optional alignment diagnostic.

--- examples/example0.cc
+++ examples/example0.cc
@@ -14,8 +14,8 @@ int main()
   cout << "1D complex to complex in-place FFT, not using the Array class" 
        << endl;
 
-  cout << endl << "FFTW requires memory alignment of " << fftw_alignment()
-       << "."  << endl;   
+//  cout << endl << "FFTW requires memory alignment of " << fftw_alignment()
+//       << "."  << endl;   
   
   unsigned int n=4; 
   Complex *f=ComplexAlign(n);
