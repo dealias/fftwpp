2ccad3ceb76b7fc6cc48270679603a305b3d710e
John Bowman <bowman@ualberta.ca>
2022-07-19 10:09:38 -0600

    Implement statistical diagnostic in hybridconv.

--- tests/hybridconv.cc
+++ tests/hybridconv.cc
@@ -1,4 +1,5 @@
 #include "convolve.h"
+#include "timing.h"
 
 #define OUTPUT 0
 #define CENTERED 0
@@ -12,6 +13,8 @@ unsigned int A=2; // number of inputs
 unsigned int B=1; // number of outputs
 unsigned int L=512; // input data length
 unsigned int M=1024; // minimum padded length
+
+int stats=MEAN; // Type of statistics used in timing test
 
 int main(int argc, char* argv[])
 {
@@ -29,6 +32,8 @@ int main(int argc, char* argv[])
 #else
   fftPad fft(L,M,FB);
 #endif
+
+  double *T=new double[K];
 
   Complex **f=new Complex *[max(A,B)];
   for(unsigned int a=0; a < A; ++a)
@@ -50,19 +55,23 @@ int main(int argc, char* argv[])
 #if OUTPUT
   K=1;
 #endif
-  double t0=totalseconds();
-
-  for(unsigned int k=0; k < K; ++k)
+  for(unsigned int k=0; k < K; ++k) {
+    seconds();
     Convolve.convolve(f,multbinary);
+    T[k]=seconds();
+  }
 
-  double t=totalseconds();
-  cout << (t-t0)/K << endl;
+  cout << endl;
+  timings("Hybrid",L,T,K,stats);
+
   cout << endl;
 #if OUTPUT
   for(unsigned int b=0; b < B; ++b)
     for(unsigned int j=0; j < L; ++j)
         cout << f[b][j] << endl;
 #endif
+
+  delete [] T;
 
   return 0;
 }
