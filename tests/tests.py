619a402fa5a289120a2abcfdb8902644eca0d5c6
noelmurasko <murasko@ualberta.ca>
2022-09-01 20:50:14 -0600

    Added counter.

--- tests/tests.py
+++ tests/tests.py
@@ -36,10 +36,16 @@ def main():
 			programs.append(Program("hybridconv3",False))
 		if HorSCH:
 			programs.append(Program("hybridconvh3",True))
+
+	tests=[0,0] # [total,failed]
 	for p in programs:
-		iterate(p,int(args.L),args.T)
+		iterate(p,int(args.L),args.T,tests)
 		print("Done "+p.name+".\n")
 
+	total=tests[0]
+	failed=tests[1]
+	passed=total-failed
+	print(str(total)+" tests done: "+str(passed)+" passed, "+str(failed)+" failed.")
 
 class Program:
 	def __init__(self, name, centered):
@@ -70,7 +76,8 @@ def getargs():
 	parser.add_argument("-L", help="L value.", default=8)
 	return parser.parse_args()
 
-def check(prog, L, M, m, D, I, T,tol=1e-8):
+def check(prog, L, M, m, D, I, T,tol,tests):
+	tests[0]+=1
 	cmd = [prog,"-L"+str(L),"-M"+str(M),"-m"+str(m),"-D"+str(D),"-I"+str(I),\
 					"-T"+str(T),"-E"]
 
@@ -84,24 +91,28 @@ def check(prog, L, M, m, D, I, T,tol=1e-8):
 	try:
 		error=float(re.search(r"(?<=Error: )(\d|\.|e|-)+",comment).group())
 		if error > tol:
+			tests[1]+=1
 			print("Error too high:")
 			print(" ".join(cmd))
 			print("Error: "+str(error))
 			print()
 	except:
+		tests[1]+=1
 		print("Error not found:")
 		print(" ".join(cmd))
 		print()
 
-def iterate(program, L=8, timeThreads=False):
-	print(L)
+def iterate(program, L, timeThreads,tests):
 	name=program.name
 	centered=program.centered
-	M0=3*L//2 if centered else 2*L-1
+
 	Dstart=2 if centered else 1
 	threads=[1,2,4] if timeThreads else [1]
+
+	M0=3*L//2 if centered else 2*L-1
+	M02=M0//2
 	for T in threads:
-		for M in range(M0,3*M0+1,L):
+		for M in [M0,3*M02,2*M0,5*M02]:
 			for m in [L//4,L//2,L,L+2,M,M+2]:
 				p=int(ceil(L/m))
 				q=int(ceil(M/m)) if p <= 2 else int(ceil(M/(p*m))*p)
@@ -110,9 +121,9 @@ def iterate(program, L=8, timeThreads=False):
 				for I in range(Istart,2):
 					D=Dstart
 					while(D < n):
-						check(name,L,M,m,D,I,T)
+						check(name,L,M,m,D,I,T,1e-8,tests)
 						D*=2
-					check(name,L,M,m,n,I,T)
+					check(name,L,M,m,n,I,T,1e-8,tests)
 
 if __name__ == "__main__":
 	main()
