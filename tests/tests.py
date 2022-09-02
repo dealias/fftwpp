733355588d70151163a3fdb251e29beeb33071cf
noelmurasko <murasko@ualberta.ca>
2022-09-01 21:06:42 -0600

    Updated code.

--- tests/tests.py
+++ tests/tests.py
@@ -26,20 +26,26 @@ def main():
 			programs.append(Program("hybridconvh",True))
 
 	if args.two or oneTwoThee:
+		pass
+		'''
 		if SorSCH:
 			programs.append(Program("hybridconv2",False))
 		if HorSCH:
 			programs.append(Program("hybridconvh2",True))
+		'''
 
 	if args.three or oneTwoThee:
+		pass
+		'''
 		if SorSCH:
 			programs.append(Program("hybridconv3",False))
 		if HorSCH:
 			programs.append(Program("hybridconvh3",True))
+		'''
 
 	tests=[0,0] # [total,failed]
 	for p in programs:
-		iterate(p,int(args.L),args.T,tests)
+		iterate(p,int(args.L),int(args.T),tests)
 		print("Done "+p.name+".\n")
 
 	total=tests[0]
@@ -72,11 +78,12 @@ def getargs():
 	parser.add_argument("-three", help="Test 3D Convolutions. Not specifying\
 												one nor two nor three is the same as specifying all of them",
 												action="store_true")
-	parser.add_argument("-T", help="Time over threads.", action="store_true")
-	parser.add_argument("-L", help="L value.", default=8)
+	parser.add_argument("-T", help="Number of threads to use in timing. If n=0, iterates over\
+											 	1, 2, and 4 threads. Default is 1.", default=1)
+	parser.add_argument("-L", help="L value. Default is 8.", default=8)
 	return parser.parse_args()
 
-def check(prog, L, M, m, D, I, T,tol,tests):
+def check(prog, L, M, m, D, I, T, tol, tests):
 	tests[0]+=1
 	cmd = [prog,"-L"+str(L),"-M"+str(M),"-m"+str(m),"-D"+str(D),"-I"+str(I),\
 					"-T"+str(T),"-E"]
@@ -102,12 +109,15 @@ def check(prog, L, M, m, D, I, T,tol,tests):
 		print(" ".join(cmd))
 		print()
 
-def iterate(program, L, timeThreads,tests):
+def iterate(program, L, thr, tests):
 	name=program.name
 	centered=program.centered
 
 	Dstart=2 if centered else 1
-	threads=[1,2,4] if timeThreads else [1]
+	if thr ==  0:
+		threads=[1,2,4]
+	else:
+		threads=[thr]
 
 	M0=3*L//2 if centered else 2*L-1
 	M02=M0//2
