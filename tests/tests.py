951c0f9bd4e265bf799497a8bb86303eaff3849e
noelmurasko <murasko@ualberta.ca>
2022-09-02 11:51:04 -0600

    Regorganized code.

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,56 @@ import re # regexp package
 import argparse
 
 def main():
-	args=getargs()
+	args=getArgs()
+
+	programs=getPrograms(args)
+
+	tests=[0,0] # [total,failed]
+	for p in programs:
+		iterate(p,int(args.L),int(args.T),tests)
+		name=p.name
+		if p.extraArgs:
+			name+=" "+p.extraArgs
+		print("Done "+name+"\n")
+
+	total=tests[0]
+	failed=tests[1]
+	passed=total-failed
+	print(str(total)+" tests done: "+str(passed)+" passed, "+str(failed)+" failed.")
+
+class Program:
+	def __init__(self, name, centered, extraArgs=""):
+		self.name=name
+		self.centered=centered
+		self.extraArgs=extraArgs
+
+def getArgs():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-S", help="Test Standard convolutions. Not specifying\
+												S nor C nor H is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-C", help="Test Centered convolutions. Not specifying\
+												S nor C nor H is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-H", help="Test Hermitian convolutions. Not specifying\
+												S nor C nor H is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-one", help="Test 1D Convolutions. Not specifying\
+												one nor two nor three is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-two", help="Test 2D Convolutions. Not specifying\
+												one nor two nor three is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-three", help="Test 3D Convolutions. Not specifying\
+												one nor two nor three is the same as specifying all of them",
+												action="store_true")
+	parser.add_argument("-T", help="Number of threads to use in timing. If n=0, iterates over\
+											 	1, 2, and 4 threads. Default is 1.", default=1)
+	parser.add_argument("-L", help="L value. Default is 8.", default=8)
+	return parser.parse_args()
+
+def getPrograms(args):
+	programs=[]
 
 	SCH=(not (args.S or args.C or args.H))
 	oneTwoThee=(not (args.one or args.two or args.three))
@@ -15,7 +64,6 @@ def main():
 	SorSCH=(args.S or SCH)
 	CorSCH=(args.C or SCH)
 	HorSCH=(args.H or SCH)
-	programs=[]
 
 	if args.one or oneTwoThee:
 		if SorSCH:
@@ -43,78 +91,7 @@ def main():
 			programs.append(Program("hybridconvh3",True))
 		'''
 
-	tests=[0,0] # [total,failed]
-	for p in programs:
-		iterate(p,int(args.L),int(args.T),tests)
-		name=p.name
-		if p.extraArgs:
-			name+=" "+p.extraArgs
-		print("Done "+name+"\n")
-
-	total=tests[0]
-	failed=tests[1]
-	passed=total-failed
-	print(str(total)+" tests done: "+str(passed)+" passed, "+str(failed)+" failed.")
-
-class Program:
-	def __init__(self, name, centered, extraArgs=""):
-		self.name=name
-		self.centered=centered
-		self.extraArgs=extraArgs
-
-def getargs():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-S", help="Test Standard convolutions. Not specifying\
-												S nor C nor H is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-C", help="Test Centered convolutions. Not specifying\
-												S nor C nor H is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-H", help="Test Hermitian convolutions. Not specifying\
-												S nor C nor H is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-one", help="Test 1D Convolutions. Not specifying\
-												one nor two nor three is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-two", help="Test 2D Convolutions. Not specifying\
-												one nor two nor three is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-three", help="Test 3D Convolutions. Not specifying\
-												one nor two nor three is the same as specifying all of them",
-												action="store_true")
-	parser.add_argument("-T", help="Number of threads to use in timing. If n=0, iterates over\
-											 	1, 2, and 4 threads. Default is 1.", default=1)
-	parser.add_argument("-L", help="L value. Default is 8.", default=8)
-	return parser.parse_args()
-
-def check(program, L, M, m, D, I, T, tol, tests):
-	tests[0]+=1
-	name=program.name
-	cmd = [name,"-L"+str(L),"-M"+str(M),"-m"+str(m),"-D"+str(D),"-I"+str(I),\
-					"-T"+str(T),"-E"]
-	if program.extraArgs != "":
-		cmd.append(program.extraArgs)
-	print(cmd)
-	vp = Popen(cmd, stdout = PIPE, stderr = PIPE)
-	vp.wait()
-	prc = vp.returncode
-
-	if prc == 0:
-		out, err = vp.communicate()
-		comment = out.rstrip().decode()
-	try:
-		error=float(re.search(r"(?<=Error: )(\d|\.|e|-)+",comment).group())
-		if error > tol:
-			tests[1]+=1
-			print("Error too high:")
-			print(" ".join(cmd))
-			print("Error: "+str(error))
-			print()
-	except:
-		tests[1]+=1
-		print("Error not found:")
-		print(" ".join(cmd))
-		print()
+	return programs
 
 def iterate(program, L, thr, tests):
 	centered=program.centered
@@ -140,6 +117,35 @@ def iterate(program, L, thr, tests):
 						check(program,L,M,m,D,I,T,1e-8,tests)
 						D*=2
 					check(program,L,M,m,n,I,T,1e-8,tests)
+
+def check(program, L, M, m, D, I, T, tol, tests):
+  tests[0]+=1
+  name=program.name
+  cmd = [name,"-L"+str(L),"-M"+str(M),"-m"+str(m),"-D"+str(D),"-I"+str(I),\
+          "-T"+str(T),"-E"]
+  if program.extraArgs != "":
+    cmd.append(program.extraArgs)
+
+  vp = Popen(cmd, stdout = PIPE, stderr = PIPE)
+  vp.wait()
+  prc = vp.returncode
+
+  if prc == 0:
+    out, err = vp.communicate()
+    comment = out.rstrip().decode()
+  try:
+    error=float(re.search(r"(?<=Error: )(\d|\.|e|-)+",comment).group())
+    if error > tol:
+      tests[1]+=1
+      print("Error too high:")
+      print(" ".join(cmd))
+      print("Error: "+str(error))
+      print()
+  except:
+    tests[1]+=1
+    print("Error not found:")
+    print(" ".join(cmd))
+    print()
 
 if __name__ == "__main__":
 	main()
