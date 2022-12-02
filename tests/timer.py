d22c463940c4bd3c380049b6237e38ee13e87982
noelmurasko <murasko@ualberta.ca>
2022-12-02 11:53:49 -0700

    Fix timer.py bugs.

--- tests/timer.py
+++ tests/timer.py
@@ -13,20 +13,28 @@ def main():
     new += "h"
   else:
     old = "c"+old
-  d=str(args.d) if args.d > 1 else ""
+  d=str(args.d) if int(args.d) > 1 else ""
   new+=d
   old+=d
   a=args.a
   b=args.b
   I=args.I
 
-  cmd1=["timing.py", "-Btaskset -c 0-15"]
+  cmd1=["timing.py"]
+  if not args.t:
+    taskset="-Btaskset -c 0"
+    if args.T > 1:
+      taskset+="-15"
+    cmd1.append(taskset)
+
   cmd2=[f"-a{a}",f"-b{b}",f"-I{I}"]
+  e=[]
   if args.e:
-    cmd2.append("-e")
-  run(cmd1+[f"-p{old}"]+cmd2+["-rimplicit"])
-  run(cmd1+[f"-p{old}"]+cmd2+["-rexplicit"])
-  run(cmd1+[f"-p{new}"]+cmd2)
+    e.append("-e")
+
+  run(cmd1+[f"-p{old}"]+cmd2+["-rimplicit"]+e)
+  run(cmd1+[f"-p{old}"]+cmd2+["-rexplicit"]+e)
+  run(cmd1+[f"-p{new}"]+cmd2+e)
   if Hermitian and I == 0:
     run(cmd1+[f"-p{new}"]+cmd2+["-rexplicit"])
 
@@ -46,6 +54,8 @@ def getArgs():
                       default=1)
   parser.add_argument("-I",help="Interval. Checks powers of 2 when 0. Default is 0.",
                       default=0)
+  parser.add_argument("-t", help="Don't use 'taskset'.",
+                      action="store_true")
   return parser.parse_args()
 
 if __name__ == "__main__":
