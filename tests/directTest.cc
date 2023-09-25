60b7ec663cd535baedda42b3d32d82332b5bffe1
noelmurasko <murasko@ualberta.ca>
2023-09-25 15:20:16 -0600

    Add direct convolution timing test.

---
+++ tests/directTest.cc
@@ -0,0 +1,61 @@
+#include <vector>
+
+#include "convolve.h"
+#include "timing.h"
+#include "direct.h"
+
+using namespace std;
+using namespace utils;
+using namespace Array;
+using namespace fftwpp;
+
+size_t A=2; // number of inputs
+size_t B=1; // number of outputs
+
+int main(int argc, char *argv[])
+{
+  L=8;  // input data length
+  M=16; // minimum padded length
+
+  optionsHybrid(argc,argv);
+
+  cout << "L=" << L << endl;
+  cout << "M=" << M << endl;
+
+  if(Output || testError)
+    K=0;
+  if(K == 0) minCount=1;
+  cout << "K=" << K << endl << endl;
+  K *= 1.0e9;
+
+  vector<double> T;
+
+  Complex **f=ComplexAlign(max(A,B),L);
+  for(size_t a=0; a < A; ++a) {
+    Complex *fa=f[a];
+    for(size_t j=0; j < L; ++j)
+      fa[j]=0.0;
+  }
+
+  Complex *h=NULL;
+
+  h=ComplexAlign(L);
+  directconv<Complex> C(L);
+
+  double sum=0.0;
+  while(sum <= K || T.size() < minCount) {
+    double t;
+    cpuTimer c;
+    C.convolve(h,f[0],f[1]);
+    t=c.nanoseconds();
+    T.push_back(t);
+    sum += t;
+  }
+
+  cout << endl;
+  timings("Direct",L,T.data(),T.size(),stats);
+  cout << endl;
+  deleteAlign(h);
+
+  return 0;
+}
