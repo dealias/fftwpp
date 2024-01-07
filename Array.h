7862a3228a2c826a82d62095b228e6810ac103a6
John Bowman <bowman@ualberta.ca>
2024-01-06 18:45:40 -0800

    Fix issue #19: clang-tidy warnings.

--- Array.h
+++ Array.h
@@ -18,7 +18,7 @@
 #ifndef __Array_h__
 #define __Array_h__ 1
 
-#define __ARRAY_H_VERSION__ 1.57
+#define __ARRAY_H_VERSION__ 1.58
 
 // Defining NDEBUG improves optimization but disables argument checking.
 // Defining __NOARRAY2OPT inhibits special optimization of Array2[].
@@ -177,7 +177,7 @@ public:
       state=unallocated;
     }
   }
-  virtual void Dimension(size_t nx0) {size=nx0;}
+  void Dimension(size_t nx0) {size=nx0;}
   void Dimension(size_t nx0, T *v0) {
     Dimension(nx0); v=v0; clear(allocated);
   }
