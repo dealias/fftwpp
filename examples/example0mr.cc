ba00c5b3434dd89e42daafa5cf4be371f6f679e7
John Bowman <bowman@ualberta.ca>
2024-07-28 16:37:45 -0700

    Generalize example of multiple 1D real-to-complex FFTs.

--- examples/example0mr.cc
+++ examples/example0mr.cc
@@ -1,6 +1,6 @@
 #define OUTPUT 0
 
-int K=100;
+int K=10;
 
 #include "Array.h"
 #include "fftw++.h"
@@ -19,32 +19,61 @@ int main()
 
   if(OUTPUT) K=1;
 
-  size_t nx=2048;
+  size_t nx=8192;
+#if OUTPUT
+  nx=8;
+#endif
   size_t ny=nx;
-  size_t nxp=nx/2+1;
   size_t align=sizeof(Complex);
 
-  size_t cstride=ny+1;
-  size_t rstride=2*cstride;
-  size_t cdist=1;
-  size_t rdist=1;
-  size_t M=ny;
+#define FFTX 1
+#define INPLACE 1
 
-#define INPLACE 0
+#if FFTX
+  size_t N=nx;
+  size_t M=ny;
+  size_t nxp=nx/2+1;
+  size_t rstride=ny+2; // Add 1 complex of padding for speed
+  size_t cstride=ny+2;
+  size_t rdist=1;
+  size_t cdist=1;
+#else
+  size_t N=ny;
+  size_t M=nx;
+  size_t nyp=ny/2+1;
+  size_t rstride=1;
+  size_t cstride=1;
+  size_t rdist=INPLACE ? 2*nyp+2 : ny+2;
+  size_t cdist=nyp;
+#endif
 
 #if INPLACE
+
+  cout << "In-place transform" << endl;
+#if FFTX
   array2<double> F(2*nxp,rstride,align);
   array2<Complex> g(nxp,cstride,(Complex *)F());
-//  double *F=(double *) g();
-  cout << "In-place transform" << endl;
 #else
-  array2<double> F(nx,rstride,align);
-  array2<Complex> g(nxp,cstride);
-  cout << "Out-of-place transform" << endl;
+  array2<double> F(nx,rdist,align);
+  array2<Complex> g(nx,cdist,(Complex *)F());
 #endif
-  double *f=(double *) F();
 
-  mrcfft1d Forward(nx, // length of transform
+#else
+
+  cout << "Out-of-place transform" << endl;
+#if FFTX
+  array2<double> F(nx,rstride,align);
+  array2<Complex> g(nxp,cstride,align);
+#else
+  array2<double> F(nx,rdist,align);
+  array2<Complex> g(nx,cdist,align);
+#endif
+
+#endif
+
+  double *f=F();
+
+  mrcfft1d Forward(N, // length of transform
                    M,  // number of transforms
                    rstride,
                    cstride,
@@ -52,7 +81,7 @@ int main()
                    cdist,
                    F,  // input array
                    g); // output array
-  mcrfft1d Backward(nx, // length of transform
+  mcrfft1d Backward(N, // length of transform
                     M,  // number of transforms
                     cstride,
                     rstride,
@@ -77,22 +106,38 @@ int main()
   }
 #endif
 
+  cpuTimer c;
+
   for(int i=0; i < K; ++i)
     Forward.fft(f,g);
+
+  double time=c.seconds();
 
 #if OUTPUT
   cout << endl << "output:" << endl;
 
+#if FFTX
   for(size_t j=0; j < ny; j++) {
     for(size_t i=0; i < nxp; i++) {
-      cout << g(cstride*i+cdist*j) << " ";
+      cout << g(cstride*i+j) << " ";
+    }
+    cout << endl;
+  }
+#else
+  for(size_t j=0; j < nyp; j++) {
+    for(size_t i=0; i < nx; i++) {
+      cout << g(cdist*i+j) << " ";
     }
     cout << endl;
   }
 #endif
 
+#endif
+
+  cpuTimer d;
   for(int i=0; i < K; ++i)
     Backward.fftNormalized(g,f);
+  time += d.seconds();
 
 #if OUTPUT
   cout << endl << "back to input:" << endl;
@@ -105,4 +150,5 @@ int main()
   }
 #endif
 
+  cout << endl << "average seconds: " << time/K << endl;
 }
